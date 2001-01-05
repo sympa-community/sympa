d6f8b43ae95b6a849e140c17b03b2882737e0f9e
salaun <salaun@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2001-01-05 16:05:38 +0000

    New comment_subscriber table field in db
    git-svn-id: https://subversion.renater.fr/sympa/trunk@28 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/etc/script/init_comment.pl
+++ src/etc/script/init_comment.pl
@@ -3,31 +3,47 @@
 use lib '--BINDIR--';
 require 'wws-lib.pl';
 
-$sympa_conf_file = '--CONFIG--';
-
 use List;
 use Log;
 
-
 ## Load sympa config
-unless (&Conf::load($sympa_conf_file)) {
-    die 'config_error';
-}
+&Conf::load('--CONFIG--') || die 'config_error';
 
 chdir $Conf::Conf{'home'};
 
 &List::db_connect() || die "Can't connect to database";
 
-my $sth =  $dbh->prepare("SELECT user_subscriber, comment_subscriber FROM subscriber_table");
+my $dbh = &List::db_get_handler();
 
-$sth = $dbh->prepare($statement) || die "Can't prepare SQL statement";
+my $sth =  $dbh->prepare("SELECT user_subscriber, comment_subscriber FROM subscriber_table") || die "Can't prepare SQL statement";
 
 $sth->execute || die "Unable to execute SQL statement";
 
 my $user;
 
 while ($user = $sth->fetchrow_hashref) {
-    printf "User: %s\n", $user->{'user_subscriber'};
+    printf "\nUser: %s", $user->{'user_subscriber'};
+
+    unless ($user->{'comment_subscriber'}) {
+	my $statement = sprintf "SELECT gecos_user FROM user_table WHERE email_user=%s", $dbh->quote($user->{'user_subscriber'});
+	my $sth2 =  $dbh->prepare($statement) || die "Can't prepare SQL statement";
+	
+	$sth2->execute || die "Unable to execute SQL statement";
+
+	my $gecos = $sth2->fetchrow;
+	$sth2->finish();
+	
+	if ($gecos) {
+	    printf " =>%s", $gecos;
+	    my $statement = sprintf "UPDATE subscriber_table SET comment_subscriber=%s WHERE (user_subscriber=%s)", $dbh->quote($gecos), $dbh->quote($user->{'user_subscriber'});
+	    my $sth2 =  $dbh->prepare($statement) || die "Can't prepare SQL statement";
+	    $sth2->execute || die "Unable to execute SQL statement";
+	    $sth2->finish();
+	}
+	
+    }
 }
+
+$sth->finish();
 
 &List::db_disconnect();
