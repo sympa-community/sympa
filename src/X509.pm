06ec56a58eba2a2d43fd1b113266ce51568eb728
salaun <salaun@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2002-08-28 06:57:18 +0000

    X509.pm currently just include subroutines for sympa to sympa https communication. All S/mime should be moved from tools.pl to X509.pm as soon as ther use openssl perl module instead of openssl commands.
    git-svn-id: https://subversion.renater.fr/sympa/trunk@1495 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

---
+++ src/X509.pm
@@ -0,0 +1,171 @@
+# X509.pm - This module includes X509-related functions (except some
+# that use openssl command line.
+#
+#<!-- RCS Identication ; $Revision$ ; $Date$ -->
+#
+#
+# Sympa - SYsteme de Multi-Postage Automatique
+# Copyright (c) 1997, 1998, 1999, 2000, 2001 Comite Reseau des Universites
+# Copyright (c) 1997,1998, 1999 Institut Pasteur & Christophe Wolfhugel
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+package X509;
+
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT = qw();
+
+require IO::Socket::SSL;
+require LWP::UserAgent;
+use Log;
+
+# request a document using https, return status and content
+sub get_https{
+	my $host = shift;
+	my $port = shift;
+	my $path = shift;
+    	my $client_cert = shift;
+	my $client_key = shift;
+	my $ssl_data= shift;
+
+	my $key_passwd = $ssl_data->{'key_passwd'};
+	my $trusted_ca_file = $ssl_data->{'cafile'};
+	my $trusted_ca_path = $ssl_data->{'capath'};
+
+	do_log ('debug','get_https (%s,%s,%s,%s,%s,%s,%s,%s)',$host,$port,$path,$client_cert,$client_key,$key_passwd,$trusted_ca_file,$trusted_ca_path );
+
+	unless ( -r ($trusted_ca_file) ||  (-d $trusted_ca_path )) {
+	    do_log ('err','error : incorrect access to both cafile and capath');
+	    return undef;
+	}
+
+	my $ssl_socket;
+
+	$ssl_socket = new IO::Socket::SSL(SSL_use_cert => 1,
+					  SSL_verify_mode => 0x01,
+					  SSL_cert_file => $client_cert,
+					  SSL_key_file => $client_key,
+					  SSL_passwd_cb => sub { return ($key_passwd)},
+					  SSL_ca_file => $trusted_ca_file,
+					  SSL_ca_path => $trusted_ca_path,
+					  PeerAddr => $host,
+					  PeerPort => $port,
+					  Proto => 'tcp',
+					  Timeout => '5'
+					  );
+	
+	unless ($ssl_socket) {
+	    do_log ('err','error %s unable to connect https://%s:%s/',&IO::Socket::SSL::errstr,$host,$port);
+	    return undef;
+	}
+	do_log ('debug','connected to https://%s:%s/',&IO::Socket::SSL::errstr,$host,$port);
+
+	if( ref($ssl_socket) eq "IO::Socket::SSL") {
+	   my $subject_name = $ssl_socket->peer_certificate("subject");
+	   my $issuer_name = $ssl_socket->peer_certificate("issuer");
+	   my $cipher = $ssl_socket->get_cipher();
+	   do_log ('debug','ssl peer certificat %s issued by %s. Cipher used %s',$subject_name,$issuer_name,$cipher);
+	}
+
+	print $ssl_socket "GET $path HTTP/1.0\n\n";
+
+	do_log ('debug',"requested GET $path HTTP/1.1");
+	#my ($buffer) = $ssl_socket->getlines;
+	# print STDERR $buffer;
+	#do_log ('debug',"return");
+	#return ;
+
+	do_log ('debug',"read answer");
+	my @result;
+	while (my $line = $ssl_socket->getline) {
+	    push  @result, $line;
+	} 
+	
+	$ssl_socket->close(SSL_no_shutdown => 1);	
+	do_log ('debug',"disconnected");
+
+	return (@result);	
+}
+
+# request a document using https, return status and content
+sub get_http2s{
+	my $host = shift;
+	my $port = shift;
+	my $path = shift;
+    	my $client_cert = shift;
+	my $client_key = shift;
+	my $ssl_data= shift;
+
+	my $key_passwd = $ssl_data->{'key_passwd'};
+	my $trusted_ca_file = $ssl_data->{'cafile'};
+	my $trusted_ca_path = $ssl_data->{'capath'};
+
+	do_log ('debug','get_https (%s,%s,%s,%s,%s,%s,%s,%s)',$host,$port,$path,$client_cert,$client_key,$key_passwd,$trusted_ca_file,$trusted_ca_path );
+
+	unless ( -r ($trusted_ca_file) ||  (-d $trusted_ca_path )) {
+	    do_log ('err','error : incorrect access to both cafile and capath');
+	    return undef;
+	}
+
+	my $ssl_socket;
+
+	$ssl_socket = new IO::Socket::SSL::context_init({SSL_use_cert => 1,
+					  SSL_verify_mode => 0x01,
+					  SSL_cert_file => $client_cert,
+					  SSL_key_file => $client_key,
+					  SSL_passwd_cb => sub { return ($key_passwd)},
+					  SSL_ca_file => $trusted_ca_file,
+					  SSL_ca_path => $trusted_ca_path,
+					  });
+	
+#	unless ($ssl_socket) {
+#	    do_log ('err','error %s unable to connect https://%s:%s/',&IO::Socket::SSL::errstr,$host,$port);
+#	    return undef;
+#	}
+
+
+	my $ua = LWP::UserAgent->new;
+	my $request = GET => 'https://'.$host.':'.$port.'/'.$path;
+
+	my $ret;
+	my $res;		
+	$res = $ua->request($request);   
+	
+       	if($res->is_success) {
+		if($res->content =~ /^CODERETOK/m) {
+			$ret =  { 	STATUS => '1',
+					CONTENT => $res->content,
+                             		ERROR => '' };
+		}
+		else {
+			$ret =  { 	STATUS => '0',
+                             		ERROR => 'Problème sur RA' };
+		}
+			
+	} 
+	else {
+		$ret =  { 	STATUS => '0',
+				ERROR => $res->status_line };
+	}
+	
+	return $ret;
+}
+
+
+#################################################################
+
+## Packages must return true.
+1;
