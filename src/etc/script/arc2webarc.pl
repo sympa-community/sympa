81ce83dae2337014902289c735eaf32c4e1bbaad
salaun <salaun@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2002-12-10 16:53:59 +0000

    [proposed by A. Bernstein] Fix: make the script VRobot-compatible
    git-svn-id: https://subversion.renater.fr/sympa/trunk@1689 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/etc/script/arc2webarc.pl
+++ src/etc/script/arc2webarc.pl
@@ -62,8 +62,9 @@ my $pinfo = &List::_apply_defaults();
 
 $| = 1;
 
-die "Usage : $ARGV[-1] <listname>" unless ($#ARGV >= 0);
+die "Usage : $ARGV[-1] <listname> [robot]" unless ($#ARGV >= 0);
 my $listname = $ARGV[0];
+my $robot = $ARGV[1];
 
 ## Check UID
 unless (getlogin() eq '--USER--') {
@@ -91,7 +92,12 @@ chdir $Conf::Conf{'home'};
 my $list = new List($listname) 
     or die 'Cannot create List object';
 
-my $home_sympa = $Conf::Conf{'home'};
+my $home_sympa;
+if ($robot) {
+    $home_sympa = $Conf::Conf{'home'}.'/'.$robot;
+}else {
+    $home_sympa = $Conf::Conf{'home'};
+}
 my $dest_dir = "$wwsconf->{'arc_path'}/$listname\@$list->{'admin'}{'host'}";
 
 ## Burst archives
