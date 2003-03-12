e3ffe26228a70631670956034bc089bb937a2e85
salaun <salaun@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2003-03-12 07:49:34 +0000

    new script "testlogs.pl" that use sympa.conf file and just add a line to logs.
    git-svn-id: https://subversion.renater.fr/sympa/trunk@1806 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

---
+++ src/etc/script/testlogs.pl
@@ -0,0 +1,35 @@
+#!--PERL--
+
+use strict;
+
+use lib '--LIBDIR--';
+use Getopt::Long;
+use Conf;
+use Log;
+
+
+my $config_file = $main::options{'config'} || '/etc/sympa.conf';
+## Load configuration file
+unless (Conf::load($config_file)) {
+   &fatal_err("Configuration file $config_file has errors.");
+}
+
+my %options;
+&GetOptions(\%main::options, 'debug|d', 'log_level=s', 'config|f=s');
+
+
+## Open the syslog and say we're read out stuff.
+do_openlog($Conf{'syslog'}, $Conf{'log_socket_type'}, 'sympa');
+
+# setting log_level using conf unless it is set by calling option
+if ($main::options{'log_level'}) {
+    do_log('info', "Logs seems OK, log level set using options : $log_level"); 
+}else{
+    $log_level = $Conf{'log_level'};
+    do_log('info', "Logs seems OK, default log level  $log_level"); 
+}
+printf "Ok, now check logs \n";
+
+1;
+
+
