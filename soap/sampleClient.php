62b6e8b498e508c1c11911080e696340778a01cc
salaun <salaun@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2003-12-18 14:30:45 +0000

    New feature: Add access to PHP source
    git-svn-id: https://subversion.renater.fr/sympa/trunk@2122 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- soap/sampleClient.php
+++ soap/sampleClient.php
@@ -20,40 +20,48 @@ $soapServer = "http://www.cru.fr/wws/wsdl";
 $soapclient = new soapclient($soapServer,'wsdl');
 
 ob_start();
-echo "<html> <body BGCOLOR=\"#000099\" TEXT=\"#cccccc\" LINK=\"#ff9933\" ALINK=\"#ff9933\" VLINK=\"#ff9933\"> \n";
 
-if ($_COOKIE['sympaEmail'] && $_COOKIE['sympaMd5']) {
+if ($_GET['viewSource']) {
 
-  // LOGOUT
-  if ($_GET['logout'] == 1) {
-    setcookie ("sympaEmail", "", time() - 3600);
-    setcookie ("sympaMd5", "", time() - 3600);
-    echo "<P ALIGN=\"center\"><FONT COLOR=\"#ff0000\">Logged out</FONT></P>\n";
-  }else {  
-    $userEmail = $_COOKIE['sympaEmail'];
-    $md5 = $_COOKIE['sympaMd5'];
-  }
+  echo "<html> <body BGCOLOR=\"#ffffff\" TEXT=\"#cccccc\" LINK=\"#ff9933\" ALINK=\"#ff9933\" VLINK=\"#ff9933\"> \n";
+  //  echo "<CODE>\n";
+  show_source ( __FILE__);
+  //echo "</CODE>\n";
+}else {
 
-  // LOGIN
-}elseif ($_POST['email'] && $_POST['pwd']) {
-  $md5 = $soapclient->call('login',array($_POST['email'],$_POST['pwd']));
-
-  if (gettype($md5) == "string") {
-    $userEmail = $_POST['email'];
+  echo "<html> <body BGCOLOR=\"#000099\" TEXT=\"#cccccc\" LINK=\"#ff9933\" ALINK=\"#ff9933\" VLINK=\"#ff9933\"> \n";
+  if ($_COOKIE['sympaEmail'] && $_COOKIE['sympaMd5']) {
     
-    setcookie("sympaEmail",$userEmail);
-    setcookie("sympaMd5",$md5);
+    // LOGOUT
+    if ($_GET['logout'] == 1) {
+      setcookie ("sympaEmail", "", time() - 3600);
+      setcookie ("sympaMd5", "", time() - 3600);
+      echo "<P ALIGN=\"center\"><FONT COLOR=\"#ff0000\">Logged out</FONT></P>\n";
+    }else {  
+      $userEmail = $_COOKIE['sympaEmail'];
+      $md5 = $_COOKIE['sympaMd5'];
+    }
+    
+    // LOGIN
+  }elseif ($_POST['email'] && $_POST['pwd']) {
+    $md5 = $soapclient->call('login',array($_POST['email'],$_POST['pwd']));
+    
+    if (gettype($md5) == "string") {
+      $userEmail = $_POST['email'];
+      
+      setcookie("sympaEmail",$userEmail);
+      setcookie("sympaMd5",$md5);
+      
+    }else {
+      echo "<P ALIGN=\"center\"><FONT COLOR=\"#ff0000\">Authentication failed</FONT></P>\n";
+    }
+  }
+  
+  if (isset($userEmail)) {
+    echo "<FONT COLOR=\"99ccff\">logged in as ".$userEmail."</FONT><BR>\n";
+    echo "[<A HREF=\"".$_SERVER['PHP_SELF']."?logout=1\">logout</A>]\n";
     
   }else {
-    echo "<P ALIGN=\"center\"><FONT COLOR=\"#ff0000\">Authentication failed</FONT></P>\n";
-  }
-}
-
-if (isset($userEmail)) {
-      echo "<FONT COLOR=\"99ccff\">logged in as ".$userEmail."</FONT><BR>\n";
-      echo "[<A HREF=\"".$_SERVER['PHP_SELF']."?logout=1\">logout</A>]\n";
-
-}else {
     echo "You need to login first :<BR><BR>\n";
     echo "<form action=\"".$_SERVER['PHP_SELF']."\" method=\"post\">
     Email:  <input type=\"text\" name=\"email\"><br>
@@ -62,79 +70,83 @@ if (isset($userEmail)) {
 </form>
 ";
   }
-
-if (isset($userEmail)) {
-
-  // SIGNOFF
-  if ($_GET['signoff'] == 1) {
-    $res = $soapclient->call('authenticateAndRun',array($userEmail,$md5,'signoff',array($_GET['list'])));
-    if (gettype($res) == 'array') {
-      echo "<P ALIGN=\"center\"><FONT COLOR=\"#ff0000\">Unsubscription failed<BR>".$res['faultstring']." : ".$res['detail']."</FONT></P>\n";
-    }else {
-      echo "<P ALIGN=\"center\"><FONT COLOR=\"#99ccff\">Successfully unsubscribed</FONT></P>\n";
-    }
-
-    // SUBSCRIBE
-  }elseif ($_GET['subscribe'] == 1) {
-    $res = $soapclient->call('authenticateAndRun',array($userEmail,$md5,'subscribe',array($_GET['list'])));
-    if (gettype($res) == 'array') {
-      echo "<P ALIGN=\"center\"><FONT COLOR=\"#ff0000\">Subscription failed<BR>".$res['faultstring']." : ".$res['detail']."</FONT></P>\n";
-    }else {
-      echo "<P ALIGN=\"center\"><FONT COLOR=\"#99ccff\">Successfully subscribed</FONT></P>\n";
-    }
-  }
-
-
-  // WHICH
-  echo "<BR><BR>Mailing lists you are subscribed to :<DL>\n";
-  //$res = $soapclient->call('authenticateAndRun',array($userEmail,$md5,'which'));
-  $res = $soapclient->call('authenticateAndRun',array($userEmail,$md5,'complexWhich'));
   
-  if (isset($res) && gettype($res) == 'array') {
+  if (isset($userEmail)) {
     
-    foreach ($res as $list) {
-      echo "<DD>";
-      list ($list['listName'],$list['listDomain']) = explode("@",$list['listAddress']);
-      $subscribed[$list['listAddress']] = True;
-      
-      echo $list['listAddress']." [<A HREF=\"".$_SERVER['PHP_SELF']."?signoff=1&list=".$list['listName']."\">signoff</A>] [<A HREF=\"".$list['homepage']."\">info</A>] \n";
-    }
-    echo "</DL>\n";
-  }else {
-    echo "<DL><DD>No subscription</DL><BR>\n";
-  }
-  
-  // LISTS
-  echo "Other mailing lists :<DL>\n";
-  $res = $soapclient->call('authenticateAndRun',array($userEmail,$md5,'complexLists'));
-  
-  if (isset($res)) {
-    foreach ($res as $list) {
-      echo "<DD>";
-      list ($list['listName'],$list['listDomain']) = explode("@",$list['listAddress']);
-      if (isset($subscribed[$list['listAddress']])) {
-	next;
+    // SIGNOFF
+    if ($_GET['signoff'] == 1) {
+      $res = $soapclient->call('authenticateAndRun',array($userEmail,$md5,'signoff',array($_GET['list'])));
+      if (gettype($res) == 'array') {
+	echo "<P ALIGN=\"center\"><FONT COLOR=\"#ff0000\">Unsubscription failed<BR>".$res['faultstring']." : ".$res['detail']."</FONT></P>\n";
       }else {
-	echo $list['listAddress']." [<A HREF=\"".$_SERVER['PHP_SELF']."?subscribe=1&list=".$list['listName']."\">subscribe</A>] [<A HREF=\"".$list['homepage']."\">info</A>] \n";
+	echo "<P ALIGN=\"center\"><FONT COLOR=\"#99ccff\">Successfully unsubscribed</FONT></P>\n";
+      }
+      
+      // SUBSCRIBE
+    }elseif ($_GET['subscribe'] == 1) {
+      $res = $soapclient->call('authenticateAndRun',array($userEmail,$md5,'subscribe',array($_GET['list'])));
+      if (gettype($res) == 'array') {
+	echo "<P ALIGN=\"center\"><FONT COLOR=\"#ff0000\">Subscription failed<BR>".$res['faultstring']." : ".$res['detail']."</FONT></P>\n";
+      }else {
+	echo "<P ALIGN=\"center\"><FONT COLOR=\"#99ccff\">Successfully subscribed</FONT></P>\n";
       }
     }
-    echo "</DL>\n";
+    
+    
+    // WHICH
+    echo "<BR><BR>Mailing lists you are subscribed to :<DL>\n";
+    //$res = $soapclient->call('authenticateAndRun',array($userEmail,$md5,'which'));
+    $res = $soapclient->call('authenticateAndRun',array($userEmail,$md5,'complexWhich'));
+    
+    if (isset($res) && gettype($res) == 'array') {
+      
+      foreach ($res as $list) {
+	echo "<DD>";
+	list ($list['listName'],$list['listDomain']) = explode("@",$list['listAddress']);
+	$subscribed[$list['listAddress']] = True;
+	
+      echo $list['listAddress']." [<A HREF=\"".$_SERVER['PHP_SELF']."?signoff=1&list=".$list['listName']."\">signoff</A>] [<A HREF=\"".$list['homepage']."\">info</A>] \n";
+      }
+      echo "</DL>\n";
+    }else {
+      echo "<DL><DD>No subscription</DL><BR>\n";
+    }
+    
+    // LISTS
+    echo "Other mailing lists :<DL>\n";
+    $res = $soapclient->call('authenticateAndRun',array($userEmail,$md5,'complexLists'));
+    
+    if (isset($res) && gettype($res) == 'array') {
+      foreach ($res as $list) {
+	echo "<DD>";
+	list ($list['listName'],$list['listDomain']) = explode("@",$list['listAddress']);
+	if (isset($subscribed[$list['listAddress']])) {
+	  next;
+	}else {
+	  echo $list['listAddress']." [<A HREF=\"".$_SERVER['PHP_SELF']."?subscribe=1&list=".$list['listName']."\">subscribe</A>] [<A HREF=\"".$list['homepage']."\">info</A>] \n";
+	}
+      }
+      echo "</DL>\n";
+    }else {
+      echo "<DL><DD>No subscription</DL><BR>\n";
+    }
+    
   }
-
 }
-
 
 ob_end_flush();
 
-unset($soapclient); ?>
+unset($soapclient); 
 
-<P ALIGN="right">
-<TABLE WIDTH="100%">
-<TR><TD ALIGN="left">
-<I><center>This is a sample PHP interface for Sympa using SOAP</center></I>
-</TD><TD ALIGN="right">
-<A HREF="http://www.sympa.org"><IMG BORDER="0" SRC="http://www.sympa.org/logos/logo-sympa-150x49.gif"></A></TD>
+echo "<P ALIGN=\"right\">
+<TABLE WIDTH=\"100%\">
+<TR><TD ALIGN=\"left\">
+<I><center>This is a sample PHP interface for Sympa using SOAP (<A HREF=\"".$_SERVER['PHP_SELF']."?viewSource=1\">View source</A>)</center></I>
+</TD><TD ALIGN=\"right\">
+<A HREF=\"http://www.sympa.org\"><IMG BORDER=\"0\" SRC=\"http://www.sympa.org/logos/logo-sympa-150x49.gif\"></A></TD>
 </TABLE>
-</P>
+</P>";
+
+?>
  </body>
  </html>
