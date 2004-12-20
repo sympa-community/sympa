19296305669916000ac1e6e37887abad9f144acc
sympa-authors <sympa-authors@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2004-12-20 09:23:55 +0000

    Fix a bug while removing .mhonarc.db file
    git-svn-id: https://subversion.renater.fr/sympa/trunk@2698 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- wwsympa/archived.pl
+++ wwsympa/archived.pl
@@ -330,7 +330,7 @@ sub rebuild {
 	    my $mm = $2 ;
 
 	    ## Remove .mhonarc.db
-	    unlink $wwsconf->{'arc_path'}.'/'.$adrlist.'/'.$yyyy.'-'.$mm.'/.mhonarc/db';
+	    unlink $wwsconf->{'arc_path'}.'/'.$adrlist.'/'.$yyyy.'-'.$mm.'/.mhonarc.db';
 	    
 	    my $cmd = "$wwsconf->{'mhonarc'} -modifybodyaddresses -addressmodifycode \'$ENV{'M2H_ADDRESSMODIFYCODE'}\'  -rcfile $mhonarc_ressources -outdir $wwsconf->{'arc_path'}/$adrlist/$yyyy-$mm  -definevars \"listname=$listname hostname=$hostname yyyy=$yyyy mois=$mm yyyymm=$yyyy-$mm wdir=$wwsconf->{'arc_path'} base=$Conf{'wwsympa_url'}/arc tag=$tag\" -umask $Conf{'umask'} $wwsconf->{'arc_path'}/$adrlist/$arc/arctxt";
 	    my $exitcode = system($cmd);
