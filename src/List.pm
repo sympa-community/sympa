01fa8447eccc46d929a5a40eb7822deb3d407646
sympa-authors <sympa-authors@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2005-05-27 14:26:06 +0000

    [BugID 418] Fix:  Ldap search return code was not examined in _include_users_ldap()
    git-svn-id: https://subversion.renater.fr/sympa/trunk@3008 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/List.pm
+++ src/List.pm
@@ -7049,14 +7049,21 @@ sub _include_users_ldap {
     do_log('debug2', "Binded to LDAP server %s ; user : '$user'", join(',',@{$host})) ;
     
     do_log('debug2', 'Searching on server %s ; suffix %s ; filter %s ; attrs: %s', join(',',@{$host}), $ldap_suffix, $ldap_filter, $ldap_attrs);
-    unless ($fetch = $ldaph->search ( base => "$ldap_suffix",
+    $fetch = $ldaph->search ( base => "$ldap_suffix",
                                       filter => "$ldap_filter",
 				      attrs => "$ldap_attrs",
-				      scope => "$param->{'scope'}")) {
-        do_log('debug2',"Unable to perform LDAP search in $ldap_suffix for $ldap_filter : $@");
+				      scope => "$param->{'scope'}");
+    unless ($fetch) {
+        do_log('err',"Unable to perform LDAP search in $ldap_suffix for $ldap_filter : $@");
         return undef;
     }
     
+    unless ($fetch->code == 0) {
+	do_log('err','Ldap search failed : %s (searching on server %s ; suffix %s ; filter %s ; attrs: %s)', 
+	       $fetch->error(), join(',',@{$host}), $ldap_suffix, $ldap_filter, $ldap_attrs);
+	return undef;
+    }
+
     ## Counters.
     my $total = 0;
     my $dn; 
@@ -8660,7 +8667,7 @@ sub get_which {
     my $email = shift;
     my $robot =shift;
     my $function = shift;
-    do_log('notice', 'List::get_which(%s, %s)', $email, $function);
+    do_log('debug2', 'List::get_which(%s, %s)', $email, $function);
 
     my ($l, @which);
 
