384e21a83c52dbb6e51c6b3bf64d5d561fdf6d3b
sympa-authors <sympa-authors@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2005-06-09 14:04:13 +0000

    Fix: adapt the code to Perl's strange behavior regarding undefined values and '== 0' test
    git-svn-id: https://subversion.renater.fr/sympa/trunk@3073 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/List.pm
+++ src/List.pm
@@ -8706,16 +8706,16 @@ sub get_which {
 
 	}elsif ($function eq 'owner') {
  	    if ($list->{'admin'}{'user_data_source'} eq 'include2'){
- 		if ($db_which->{'owner'}{$l} == 1) {
+		if (! defined $db_which->{'owner'}{$l}) {
+		    push @which, $list->{'name'} if ($list->am_i('owner',$email));
+		}elsif ($db_which->{'owner'}{$l} == 1) {
  		    push @which, $l ;
-
+		    
 		    ## Update cache
 		    $list_cache{'am_i'}{'owner'}{$l}{$email} = 1;
 		}elsif ($db_which->{'owner'}{$l} == 0) {
 		    ## Update cache
 		    $list_cache{'am_i'}{'owner'}{$l}{$email} = 0;		    
-		}else {
-		    push @which, $list->{'name'} if ($list->am_i('owner',$email));
 		}
  	    }else {	    
  		push @which, $list->{'name'} if ($list->am_i('owner',$email));
@@ -8723,9 +8723,11 @@ sub get_which {
 
 	}elsif ($function eq 'editor') {
  	    if ($list->{'admin'}{'user_data_source'} eq 'include2'){
- 		if ($db_which->{'editor'}{$l} == 1) {
+		if (! defined $db_which->{'editor'}{$l}) {
+		    push @which, $list->{'name'} if ($list->am_i('editor',$email));	       
+		}elsif ($db_which->{'editor'}{$l} == 1) {
  		    push @which, $l ;
- 
+		    
 		    ## Update cache
 		    $list_cache{'am_i'}{'editor'}{$l}{$email} = 1;
  		}elsif ($db_which->{'editor'}{$l} == 0) {
