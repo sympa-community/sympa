ec29c0558a120fa15c70478369c0a418d5b53c08
sympa-authors <sympa-authors@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2006-08-03 12:45:00 +0000

    Fix: connection to DB would fail
    git-svn-id: https://subversion.renater.fr/sympa/trunk@3811 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/Log.pm
+++ src/Log.pm
@@ -130,6 +130,7 @@ sub get_log_date {
     ## Check database connection
     unless ($dbh and $dbh->ping) {
 	return undef unless &List::db_connect();
+	$dbh = &List::db_get_handler();
     }
 
     my $statement;
@@ -194,6 +195,7 @@ sub db_log {
     ## Check database connection
     unless ($dbh and $dbh->ping) {
 	return undef unless &List::db_connect();
+	$dbh = &List::db_get_handler();
     }
 
     unless ($daemon =~ /^((task)|(archived)|(sympa)|(wwsympa)|(bounced)|(sympa_soap))$/) {
@@ -279,6 +281,7 @@ sub get_first_db_log {
     ## Check database connection
     unless ($dbh and $dbh->ping) {
 	return undef unless &List::db_connect();
+	$dbh = &List::db_get_handler();
     }
 
     my $statement = sprintf "SELECT date_logs AS date, robot_logs AS robot, list_logs AS list, action_logs AS action, parameters_logs AS parameters, target_email_logs AS target_email,msg_id_logs AS msg_id, status_logs AS status, error_type_logs AS error_type, user_email_logs AS user_email, client_logs AS client, daemon_logs AS daemon FROM logs_table WHERE robot_logs=%s ", $dbh->quote($select->{'robot'});	
