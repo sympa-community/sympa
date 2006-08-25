39fbb8519b4eca20c6d861e23835e2754be8d90f
sympa-authors <sympa-authors@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2006-08-25 16:06:24 +0000

    [reported by D.Ghent, UMBC] [ID 927]Fix: arc2webarc.pl failed with big lists (lots of messages) because all messages were stored in memory. Now process one log file at a time.
    git-svn-id: https://subversion.renater.fr/sympa/trunk@3861 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/etc/script/arc2webarc.pl
+++ src/etc/script/arc2webarc.pl
@@ -49,8 +49,8 @@ my %month_idx = qw(jan 1
 		   dec 12
 		   dc  12);
 
-my @msgs;
 my %nummsg;
+my $msg_count;
 
 my %options;
 # load options ?
@@ -100,7 +100,6 @@ if ($robot) {
 }
 my $dest_dir = $wwsconf->{'arc_path'}.'/'.$list->get_list_id();
 
-## Burst archives
 unless (-d "$home_sympa/$listname") {
     die "No directory for list $listname";
 }
@@ -109,11 +108,22 @@ unless (-d "$home_sympa/$listname/archives") {
     die "No archives for list $listname";
 }
 
+if (-d $dest_dir) {
+    print "Web archives already exist for list $listname\nGo on (<CR>|n) ?";
+    my $s = <STDIN>;
+    die if ($s eq 'n');
+}else {
+    mkdir $dest_dir, 0755 or die;
+}
+
+
 print STDERR "Bursting archives\n";
 foreach my $arc_file (<$home_sympa/$listname/archives/log*>) {
     my ($first, $new);
     my $msg = [];
+    my @msgs;
 
+    ## Split the archives file
     print '.';
     open ARCFILE, $arc_file;
     while (<ARCFILE>) {
@@ -127,6 +137,7 @@ foreach my $arc_file (<$home_sympa/$listname/archives/log*>) {
 	    next;
 	}elsif (/^------- CUT --- CUT/) {
 	    push @msgs, $msg;
+	    $msg_count++;
 	    $msg = [];
 	    $new = 1;
 	}else {
@@ -135,120 +146,113 @@ foreach my $arc_file (<$home_sympa/$listname/archives/log*>) {
 	}
     }
     close ARCFILE;
-}
 
-print STDERR "\nFound $#msgs messages\n";
+    ##Dump
+    #foreach my $i (0..$#msgs) {
+    #    printf "******** Message %d *******\n", $i;
+    #    print @{$msgs[$i]};
+    #}
 
-##Dump
-#foreach my $i (0..$#msgs) {
-#    printf "******** Message %d *******\n", $i;
-#    print @{$msgs[$i]};
-#}
-
-
-if (-d $dest_dir) {
-    print "Web archives already exist for list $listname\nGo on (<CR>|n) ?";
-    my $s = <STDIN>;
-    die if ($s eq 'n');
-}else {
-    mkdir $dest_dir, 0755 or die;
-}
-
-## Analyzing Date header fields
-
-print STDERR "Analysing Date: header fields\n";
-foreach my $msg (@msgs) {
-    my $incorrect = 0;
-    my ($date, $year, $month);
-    
-    print '.';
-    foreach (@{$msg}) {
-	if (/^Date:\s+(.*)$/) {
-	    #print STDERR "#$_#\n";
-	    $date = $1;
-
-	    # Date type : Mon, 8 Dec 97 13:33:47 +0100
-	    if ($date =~ /^\w{2,3},\s+\d{1,2}\s+([\wéû]{2,3})\s+(\d{2,4})/) {
-                $month = $1;
-		$year =$2;
-		#print STDERR "$month/$year\n";
-
-	    # Date type : 8 Dec 97 13:33:47+0100
-	    }elsif ($date =~ /^\d{1,2}\s+(\w{3}) (\d{2,4})/) {
-		$month = $1;
-		$year =$2;
-
-	    # Date type : 8-DEC-1997 13:33:47 +0100
-	    }elsif ($date =~ /^\d{1,2}-(\w{3})-(\d{4})/) {
-		$month = $1;
-		$year =$2;
-
-	    # Date type : Mon Dec 8 13:33:47 1997
-	    }elsif ($date =~ /^\w+\s+(\w+)\s+\d{1,2} \d+:\d+:\d+ (GMT )?(\d{4})/) {
-		$month = $1;
-		$year =$3;
-
-	    # unknown date format
-	    }else {
-		$incorrect = 1;
+    ## Analyzing Date header fields
+    #print STDERR "Analysing Date: header fields\n";
+    foreach my $msg (@msgs) {
+	my $incorrect = 0;
+	my ($date, $year, $month);
+	
+	print '.';
+	foreach (@{$msg}) {
+	    if (/^Date:\s+(.*)$/) {
+		#print STDERR "#$_#\n";
+		$date = $1;
+		
+		# Date type : Mon, 8 Dec 97 13:33:47 +0100
+		if ($date =~ /^\w{2,3},\s+\d{1,2}\s+([\wéû]{2,3})\s+(\d{2,4})/) {
+		    $month = $1;
+		    $year =$2;
+		    #print STDERR "$month/$year\n";
+		    
+		    # Date type : 8 Dec 97 13:33:47+0100
+		}elsif ($date =~ /^\d{1,2}\s+(\w{3}) (\d{2,4})/) {
+		    $month = $1;
+		    $year =$2;
+		    
+		    # Date type : 8-DEC-1997 13:33:47 +0100
+		}elsif ($date =~ /^\d{1,2}-(\w{3})-(\d{4})/) {
+		    $month = $1;
+		    $year =$2;
+		    
+		    # Date type : Mon Dec 8 13:33:47 1997
+		}elsif ($date =~ /^\w+\s+(\w+)\s+\d{1,2} \d+:\d+:\d+ (GMT )?(\d{4})/) {
+		    $month = $1;
+		    $year =$3;
+		    
+		    # unknown date format
+		}else {
+		    $incorrect = 1;
+		    last;
+		}
+		
+		# Month format
+		if ($month !~ /^\d+$/) {
+		    $month =~ y/éûA-Z/eua-z/;
+		    if (!$month_idx{$month}) {
+			$incorrect = 1;
+		    }else {
+			$month = $month_idx{$month};
+		    }
+		}elsif (($month < 1) or ($month > 12)) {
+		    $incorrect = 1;
+		}
+		$month = "0".$month if $month =~ /^\d$/;
+		
+		# Checking Year format
+		if ($year =~ /^[89]\d$/) {
+		    $year = "19".$year;
+		}elsif ($year !~ /^19[89]\d|200[0-9]$/) {
+		    $incorrect = 1;
+		}
+		
 		last;
 	    }
-               
-	    # Month format
-	    if ($month !~ /^\d+$/) {
-		$month =~ y/éûA-Z/eua-z/;
-		if (!$month_idx{$month}) {
-		    $incorrect = 1;
-		}else {
-		    $month = $month_idx{$month};
-		}
-	    }elsif (($month < 1) or ($month > 12)) {
-		$incorrect = 1;
-	    }
-	    $month = "0".$month if $month =~ /^\d$/;
 	    
-	    # Checking Year format
-	    if ($year =~ /^[89]\d$/) {
-		$year = "19".$year;
-	    }elsif ($year !~ /^19[89]\d|200[0-9]$/) {
-		$incorrect = 1;
+	    # empty line => end of header
+	    if (/^\s*$/) {
+		last;
 	    }
-	    
-	    last;
+	}
+	close MSG;
+	# Unknown date format/No date
+	if ($incorrect || ! $month || ! $year) {
+	    $year = 'UN';
+	    $month = 'KNOWN';
 	}
 	
-	# empty line => end of header
-	if (/^\s*$/) {
-	    last;
+	# New month
+	if (!-d "$dest_dir/$year-$month") {
+	    print "\nNew directory $year-$month\n";
+	    `mkdir $dest_dir/$year-$month`;
 	}
+	
+	if (!-d "$dest_dir/$year-$month/arctxt") {
+	    `mkdir $dest_dir/$year-$month/arctxt`;
+	}
+	
+	$nummsg{$year}{$month}++ while (-e "$dest_dir/$year-$month/arctxt/$nummsg{$year}{$month}");
+	
+	# Save message
+	open DESTFILE, ">$dest_dir/$year-$month/arctxt/$nummsg{$year}{$month}";
+	print DESTFILE @{$msg};
+	close DESTFILE;
+	#    `mv $m $dest_dir/$year-$month/arctxt/$nummsg{$year}{$month}`;
+	$nummsg{$year}{$month}++;
     }
-    close MSG;
-    # Unknown date format/No date
-    if ($incorrect || ! $month || ! $year) {
-	$year = 'UN';
-	$month = 'KNOWN';
-    }
-    
-    # New month
-    if (!-d "$dest_dir/$year-$month") {
-	print "\nNew directory $year-$month\n";
-	`mkdir $dest_dir/$year-$month`;
-    }
-
-    if (!-d "$dest_dir/$year-$month/arctxt") {
-	`mkdir $dest_dir/$year-$month/arctxt`;
-    }
-
-    $nummsg{$year}{$month}++ while (-e "$dest_dir/$year-$month/arctxt/$nummsg{$year}{$month}");
-
-    # Save message
-    open DESTFILE, ">$dest_dir/$year-$month/arctxt/$nummsg{$year}{$month}";
-    print DESTFILE @{$msg};
-    close DESTFILE;
-#    `mv $m $dest_dir/$year-$month/arctxt/$nummsg{$year}{$month}`;
-     $nummsg{$year}{$month}++;
-}
   
+
+}
+
+print STDERR "\nFound $msg_count messages\n";
+
+
 ## Rebuild web archives
 print STDERR "Rebuilding HTML\n";
 my $list_id = $list->get_list_id();
