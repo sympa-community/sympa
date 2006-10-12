b21c8f4e431fe49a73ab99bbefb75c02302e73da
sympa-authors <sympa-authors@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2006-10-12 09:58:32 +0000

    Fixed typo
    git-svn-id: https://subversion.renater.fr/sympa/trunk@3938 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- wwsympa/bounced.pl
+++ wwsympa/bounced.pl
@@ -372,7 +372,7 @@ while (!$end) {
 		$forward = 'listmaster';
 		do_log ('err','ignoring Feedback Report %s : unknown format (feedback_type:%s, original_rcpt:%s, listname:%s)',$file, $feedback_type, $original_rcpt, $listname );		
 	    }
-	    $listname |= 'sympa';
+	    $listname ||= 'sympa';
 	    &DoForward($listname, $forward, $robot, $entity) if (defined $forward);
 
 	    unlink("$queue/$file");
