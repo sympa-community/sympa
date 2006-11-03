6a9d2dcdbec0de402e3152abd1a94eb57d827562
sympa-authors <sympa-authors@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2006-11-03 08:39:15 +0000

    [S.Ikeda] Fix: restore FB_CROAK to preserve data if operation fails
    git-svn-id: https://subversion.renater.fr/sympa/trunk@3993 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/tt2.pl
+++ src/tt2.pl
@@ -177,8 +177,10 @@ sub decode_utf8 {
 
     ## Skip if already internally tagged utf8
     unless (&Encode::is_utf8($string)) {
+	## Wrapped with eval to prevent Sympa process from dying
+	## FB_CROAK is used instead of FB_WARN to pass $string intact to succeeding processes it operation fails
 	eval {
-	    $string = &Encode::decode_utf8($string, Encode::FB_WARN);
+	    $string = &Encode::decode_utf8($string, Encode::FB_CROAK);
 	};
 	$@ = '';
     }
