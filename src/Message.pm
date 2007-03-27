d45cc91e9a62dbe6c3f415c6490f92cbbece1654
david.verdin <david.verdin@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2007-03-27 11:53:13 +0000

    Added pod comments to Message.pm
    git-svn-id: https://subversion.renater.fr/sympa/trunk@4203 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/Message.pm
+++ src/Message.pm
@@ -20,6 +20,18 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
+=pod 
+
+=head1 NAME 
+
+I<Message.pm> - mail message embedding for internal use in Sympa
+
+=head1 DESCRIPTION 
+
+While processing a message in Sympa, we need to link informations to rhe message, mdify headers and such. This was quite a problem when a message was signed, as modifying anything in the message body would alter its MD5 footprint. And probably make the message to be rejected by clients verifying its identity (which is somehow a good thing as it is the reason why people use MD5 after all). With such messages, the process was complex. We then decided to embed any message treated in a "Message" object, thus making the process easier.
+
+=cut 
+
 package Message;
 
 use strict;
@@ -40,6 +52,75 @@ use MIME::EncWords;
 use MIME::Parser;
 use Conf;
 use Log;
+
+=pod 
+
+=head1 SUBFUNCTIONS 
+
+This is the description of the subfunctions contained by Message.pm
+
+=cut 
+
+
+=pod 
+
+=head2 sub new
+
+Creates a new Message object.
+
+=head3 Arguments 
+
+=over 
+
+=item * I<$pkg>, a package name 
+
+=item * I<$file>, the message file
+
+=item * I<$noxsympato>, a boolean
+
+=back 
+
+=head3 Return 
+
+=over 
+
+=item * I<a Message object>, if created
+
+=item * I<undef>, if something went wrong
+
+=back 
+
+=head3 Calls 
+
+=over 
+
+=item * do_log
+
+=item * Conf::get_robot_conf
+
+=item * List::new
+
+=item * Mail::Address::parse
+
+=item * MIME::EncWords::decode_mimewords
+
+=item * MIME::Entity::as_string
+
+=item * MIME::Head::get
+
+=item * MIME::Parser::output_to_core
+
+=item * MIME::Parser::read
+
+=item * tools::valid_email
+
+=item * tools::smime_decrypt
+
+=item * tools::smime_sign_check
+
+=back 
+
+=cut 
 
 ## Creates a new object
 sub new {
@@ -196,6 +277,40 @@ sub new {
     return $message;
 }
 
+=pod 
+
+=head2 sub dump
+
+Dump a Message object to a stream.
+
+=head3 Arguments 
+
+=over 
+
+=item * I<$self>, the Message object to dump
+
+=item * I<$output>, the stream to which dump the object
+
+=back 
+
+=head3 Return 
+
+=over 
+
+=item * I<1>, if everything's alright
+
+=back 
+
+=head3 Calls 
+
+=over 
+
+=item * None
+
+=back 
+
+=cut 
+
 ## Dump the Message object
 sub dump {
     my ($self, $output) = @_;
@@ -218,6 +333,40 @@ sub dump {
     return 1;
 }
 
+=pod 
+
+=head2 sub add_topic
+
+Add topic and put header X-Sympa-Topic.
+
+=head3 Arguments 
+
+=over 
+
+=item * I<$self>, the Message object to which add a topic
+
+=item * I<$output>, the string containing the topic to add
+
+=back 
+
+=head3 Return 
+
+=over 
+
+=item * I<1>, if everything's alright
+
+=back 
+
+=head3 Calls 
+
+=over 
+
+=item * MIME::Head::add
+
+=back 
+
+=cut 
+
 ## Add topic and put header X-Sympa-Topic
 sub add_topic {
     my ($self,$topic) = @_;
@@ -229,6 +378,40 @@ sub add_topic {
     return 1;
 }
 
+
+=pod 
+
+=head2 sub add_topic
+
+Add topic and put header X-Sympa-Topic.
+
+=head3 Arguments 
+
+=over 
+
+=item * I<$self>, the Message object whose topic is retrieved
+
+=back 
+
+=head3 Return 
+
+=over 
+
+=item * I<the topic>, if it exists
+
+=item * I<empty string>, otherwise
+
+=back 
+
+=head3 Calls 
+
+=over 
+
+=item * MIME::Head::add
+
+=back 
+
+=cut 
 
 ## Get topic
 sub get_topic {
@@ -245,3 +428,16 @@ sub get_topic {
 
 ## Packages must return true.
 1;
+=pod 
+
+=head1 AUTHORS 
+
+=over 
+
+=item * Serge Aumont <sa AT cru.fr> 
+
+=item * Olivier Salaün <os AT cru.fr> 
+
+=back 
+
+=cut 
