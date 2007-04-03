ed75bb8e430b8a77ec3802ff6d311f3c9b9b42e7
david.verdin <david.verdin@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2007-04-03 07:19:22 +0000

    [Reported by T. Berry, Nasa] Fixed a few typos in the web gui and in console messages.
    git-svn-id: https://subversion.renater.fr/sympa/trunk@4222 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/tools.pl
+++ src/tools.pl
@@ -1802,7 +1802,7 @@ sub get_filename {
 	}
 	my @result;
 	foreach my $f (@try) {
-	    &do_log('debug3','get_filname : name: %s ; dir %s', $name, $f  );
+	    &do_log('debug3','get_filename : name: %s ; dir %s', $name, $f  );
 	    if (-r $f) {
 		if ($options->{'order'} eq 'all') {
 		    push @result, $f;
