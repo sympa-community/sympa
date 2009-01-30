9db08f216b76e3bbb27a9239d0c9829da88b2299
olivier.salaun <olivier.salaun@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2009-01-30 10:10:53 +0000

    [change] now using the standard php-soap library instead of nusoap
    git-svn-id: https://subversion.renater.fr/sympa/trunk@5470 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- soap/sampleClient.php
+++ soap/sampleClient.php
@@ -1,13 +1,10 @@
 <?php 
-// Just an example of a PHP SOAP client for Sympa. You can change to another web service
-// of Sympa without difficulty (look at the API of nusoap if you want to learn more).
+// Just a simple example of a PHP SOAP client for Sympa. 
+// You can check the Sympa SOAP API definition to design more complex code : http://www.sympa.org/manual/soap
 //
-// This need the SOAP library for PHP located at : http://dietrich.ganx4.com/nusoap
-// You also need to customize the path to nusoap.php and the $soapServer variable
+// You need to install the php-soap module. 
+// If you are on a Fedora Core server, you can run "yum install php-soap"
 
-include('/home/httpd/test/nusoap.php');
-
-require_once('nusoap.php');
 
 $soapclient->debug_flag=true;
 
@@ -16,17 +13,14 @@ global $md5;
 global $soapServer;
 $soapServer = "http://demo.sympa.org/sympa/wsdl";
 
-// $parameters = array($list,$email);
-$soapclient = new soapclient($soapServer,'wsdl');
+$soapclient = new SoapClient($soapServer);
 
 ob_start();
 
 if ($_GET['viewSource']) {
 
   echo "<html> <body BGCOLOR=\"#ffffff\" TEXT=\"#cccccc\" LINK=\"#ff9933\" ALINK=\"#ff9933\" VLINK=\"#ff9933\"> \n";
-  //  echo "<CODE>\n";
   show_source ( __FILE__);
-  //echo "</CODE>\n";
 }else {
 
   echo "<html> <body BGCOLOR=\"#000099\" TEXT=\"#cccccc\" LINK=\"#ff9933\" ALINK=\"#ff9933\" VLINK=\"#ff9933\"> \n";
@@ -44,7 +38,7 @@ if ($_GET['viewSource']) {
     
     // LOGIN
   }elseif ($_POST['email'] && $_POST['pwd']) {
-    $md5 = $soapclient->call('login',array($_POST['email'],$_POST['pwd']));
+    $md5 = $soapclient->login($_POST['email'],$_POST['pwd']);
     
     if (gettype($md5) == "string") {
       $userEmail = $_POST['email'];
@@ -62,10 +56,6 @@ if ($_GET['viewSource']) {
     echo "[<A HREF=\"".$_SERVER['PHP_SELF']."?logout=1\">logout</A>]\n";
     
   }else {
-    echo "This PHP script shows how you can easily provide access to a Sympa mailing list service from another server, using a different programming language using web services.<BR><BR>
-This script does not access Sympa data, nor does it reinvent the mailing list server's job. It does remote high level procedure calls on Sympa server using <A HREF=\"http://www.w3.org/2002/ws/\">SOAP</A>. A Sympa SOAP server is running on the server ; it currently implements a subset of Sympa features including Login, Subscription, Signoff, Lists, Which,... The list of available remote procedures are published in a WSDL document that is fetched by the SOAP client.<BR><BR>
-Read Sympa documentation about the SOAP server for more details.<BR><BR><HR>";
-
     echo "You need to login first :<BR><BR>\n";
     echo "<form action=\"".$_SERVER['PHP_SELF']."\" method=\"post\">
     Email:  <input type=\"text\" name=\"email\"><br>
@@ -79,7 +69,7 @@ Read Sympa documentation about the SOAP server for more details.<BR><BR><HR>";
     
     // SIGNOFF
     if ($_GET['signoff'] == 1) {
-      $res = $soapclient->call('authenticateAndRun',array($userEmail,$md5,'signoff',array($_GET['list'])));
+      $res = $soapclient->authenticateAndRun($userEmail,$md5,'signoff',array($_GET['list']));
       if (gettype($res) == 'array') {
 	echo "<P ALIGN=\"center\"><FONT COLOR=\"#ff0000\">Unsubscription failed<BR>".$res['faultstring']." : ".$res['detail']."</FONT></P>\n";
       }else {
@@ -88,7 +78,7 @@ Read Sympa documentation about the SOAP server for more details.<BR><BR><HR>";
       
       // SUBSCRIBE
     }elseif ($_GET['subscribe'] == 1) {
-      $res = $soapclient->call('authenticateAndRun',array($userEmail,$md5,'subscribe',array($_GET['list'])));
+      $res = $soapclient->authenticateAndRun($userEmail,$md5,'subscribe',array($_GET['list']));
       if (gettype($res) == 'array') {
 	echo "<P ALIGN=\"center\"><FONT COLOR=\"#ff0000\">Subscription failed<BR>".$res['faultstring']." : ".$res['detail']."</FONT></P>\n";
       }else {
@@ -99,17 +89,16 @@ Read Sympa documentation about the SOAP server for more details.<BR><BR><HR>";
     
     // WHICH
     echo "<BR><BR>Mailing lists you are subscribed to :<DL>\n";
-    //$res = $soapclient->call('authenticateAndRun',array($userEmail,$md5,'which'));
-    $res = $soapclient->call('authenticateAndRun',array($userEmail,$md5,'complexWhich'));
+    $res = $soapclient->authenticateAndRun($userEmail,$md5,'complexWhich');
     
     if (isset($res) && gettype($res) == 'array') {
       
       foreach ($res as $list) {
 	echo "<DD>";
-	list ($list['listName'],$list['listDomain']) = explode("@",$list['listAddress']);
-	$subscribed[$list['listAddress']] = True;
+	list ($list->listName,$list->listDomain) = explode("@",$list->listAddress);
+	$subscribed[$list->listAddress] = True;
 	
-      echo "<P>".$list['listAddress']." [<A HREF=\"".$_SERVER['PHP_SELF']."?signoff=1&list=".$list['listName']."\">signoff</A>] [<A HREF=\"".$list['homepage']."\">info</A>]<BR>".$list['subject']."</P>\n";
+      echo "<P>".$list->listAddress." [<A HREF=\"".$_SERVER['PHP_SELF']."?signoff=1&list=".$list->listName."\">signoff</A>] [<A HREF=\"".$list->homepage."\">info</A>]<BR>".$list->subject."</P>\n";
       }
       echo "</DL>\n";
     }else {
@@ -118,16 +107,16 @@ Read Sympa documentation about the SOAP server for more details.<BR><BR><HR>";
     
     // LISTS
     echo "Other mailing lists :<DL>\n";
-    $res = $soapclient->call('authenticateAndRun',array($userEmail,$md5,'complexLists'));
+    $res = $soapclient->authenticateAndRun($userEmail,$md5,'complexLists');
     
     if (isset($res) && gettype($res) == 'array') {
       foreach ($res as $list) {
 	echo "<DD>";
-	list ($list['listName'],$list['listDomain']) = explode("@",$list['listAddress']);
-	if (isset($subscribed[$list['listAddress']])) {
+	list ($list->listName,$list->listDomain) = explode("@",$list->listAddress);
+	if (isset($subscribed[$list->listAddress])) {
 	  next;
 	}else {
-	  echo "<P>".$list['listAddress']." [<A HREF=\"".$_SERVER['PHP_SELF']."?subscribe=1&list=".$list['listName']."\">subscribe</A>] [<A HREF=\"".$list['homepage']."\">info</A>]<BR>".$list['subject']." \n</P>";
+	  echo "<P>".$list->listAddress." [<A HREF=\"".$_SERVER['PHP_SELF']."?subscribe=1&list=".$list->listName."\">subscribe</A>] [<A HREF=\"".$list->homepage."\">info</A>]<BR>".$list->subject." \n</P>";
 	}
       }
       echo "</DL>\n";
