3d4a3e16058aedc2d40037fed282d44d63a3be52
olivier.salaun <olivier.salaun@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2009-08-07 09:29:29 +0000

    [bug][#6380][Reported by M.Warkus, Philipps-Univ. Marburg] Added a line break to written PID files. Without the linebreak, the init script failed to check process status on OS without /etc/init.d/functions
    git-svn-id: https://subversion.renater.fr/sympa/trunk@6069 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/tools.pm
+++ src/lib/tools.pm
@@ -2259,7 +2259,7 @@ sub remove_pid {
 		unless (open(PFILE, ">$pidfile")) {
 		    fatal_err('Could not open %s, exiting', $pidfile);
 		}
-		print PFILE "$previous_pid";
+		print PFILE "$previous_pid\n";
 		close(PFILE);
 	    }else{
 		&do_log('notice','pidfile %s does not exist. Nothing to do.',$pidfile);
@@ -2327,11 +2327,15 @@ sub write_pid {
     }
 
     ## If we can have multiple options for the process.
+    ## Print other pids + this one
     if($options->{'multiple_process'}){
-	unless (open(LCK, ">> $pidfile")) {
+	unless (open(LCK, "> $pidfile")) {
 	    fatal_err('Could not open %s, exiting', $pidfile);
 	}
-	print LCK "$pid ";
+
+	## Print other pids + this one
+	print LCK "$other_pid $pid\n";
+
 	close(LCK);
     }else{
 	## Create and write the pidfile
@@ -2367,7 +2371,7 @@ sub write_pid {
 	    fatal_err('Could not truncate %s, exiting.', $pidfile);
 	}
 	
-	print LCK "$pid ";
+	print LCK "$pid\n";
 	close(LCK);
     }
     unless (&tools::set_file_rights(file => $pidfile,
@@ -2481,11 +2485,6 @@ sub remove_dir {
     do_log('debug2','remove_dir()');
     
     foreach my $current_dir (@_){
-	my @tree = split /\//, $current_dir ;
-	if ($#tree < 4) {
-	    do_log('err',"$current_dir not removed (not enough / in directory name)");
-	    next;
-	}
 	finddepth({wanted => \&del, no_chdir => 1},$current_dir);
     }
     sub del {
