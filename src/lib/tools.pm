f29b55643fd4c4485d3d054c5deb850259bdc37e
olivier.salaun <olivier.salaun@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2010-06-16 13:48:01 +0000

    [bug][S.Ikeda][#3779] Comma in gecos caused invalid From: in Web posting. New tools::addrencode() for proper encoding of From: SMTP header field according to RFC5322.
    git-svn-id: https://subversion.renater.fr/sympa/trunk@6529 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/tools.pm
+++ src/lib/tools.pm
@@ -3883,5 +3883,38 @@ sub wrap_text {
     return $text;
 }
 
+#*******************************************
+# Function : addrencode
+# Description : return formatted (and encoded) name-addr as RFC5322 3.4.
+## IN : addr, [phrase, [charset]]
+#*******************************************
+sub addrencode {
+    my $addr = shift;
+    my $phrase = (shift || '');
+    my $charset = (shift || 'utf8');
+
+    return undef unless $addr =~ /\S/;
+
+    if ($phrase =~ /[^\s\x21-\x7E]/) {
+	# Minimal encoding leaves special characters unencoded.
+	# In this case do maximal encoding for workaround.
+	my $minimal =
+	    ($phrase =~ /(\A|\s)[\x21-\x7E]*[\"(),:;<>\@\\][\x21-\x7E]*(\s|\z)/)?
+	    'NO': 'YES';
+	$phrase = MIME::EncWords::encode_mimewords(
+	    Encode::decode('utf8', $phrase),
+	    'Encoding' => 'A', 'Charset' => $charset,
+	    'Replacement' => 'FALLBACK',
+	    'Field' => 'Resent-Sender', # almost longest
+	    'Minimal' => $minimal
+            );
+	return "$phrase <$addr>";
+    } elsif ($phrase =~ /\S/) {
+	$phrase =~ s/([\\\"])/\\$1/g;
+	return "\"$phrase\" <$addr>";
+    } else {
+	return "<$addr>";
+    }
+}
 
 1;
