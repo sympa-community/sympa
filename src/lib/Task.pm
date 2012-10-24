0b7912e51f00d2dbb21a51a02a535f9acf31d1de
david.verdin <david.verdin@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2012-10-24 09:15:42 +0000

    [dev] Refactoring. Moving all subs and global vars related to the manipulation of the task spool (getting list of tasks, creating missing tasks, etc.) to a specific module named TaskSpool.pm.
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-6.2-branch@7797 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/Task.pm
+++ src/lib/Task.pm
@@ -37,40 +37,10 @@ use Log;
 use mail;
 use Scenario;
 use Sympaspool;
+use TaskSpool;
 use tools;
 use tracking;
 use tt2;
-
-my @task_list;
-my %task_by_list;
-my %task_by_model;
-
-my $taskspool ;
-
-my @tasks; # list of tasks in the spool
-
-## list of list task models
-#my @list_models = ('expire', 'remind', 'sync_include');
-our @list_models = ('sync_include','remind');
-
-## hash of the global task models
-our %global_models = (#'crl_update_task' => 'crl_update', 
-		     #'chk_cert_expiration_task' => 'chk_cert_expiration',
-		     'expire_bounce_task' => 'expire_bounce',
-		     'purge_user_table_task' => 'purge_user_table',
-		     'purge_logs_table_task' => 'purge_logs_table',
-		     'purge_session_table_task' => 'purge_session_table',
-		     'purge_tables_task' => 'purge_tables',
-		     'purge_one_time_ticket_table_task' => 'purge_one_time_ticket_table',
-		     'purge_orphan_bounces_task' => 'purge_orphan_bounces',
-		     'eval_bouncers_task' => 'eval_bouncers',
-		     'process_bouncers_task' =>'process_bouncers',
-		     #,'global_remind_task' => 'global_remind'
-		     );
-
-## month hash used by epoch conversion routines
-our %months = ('Jan', 0, 'Feb', 1, 'Mar', 2, 'Apr', 3, 'May', 4,  'Jun', 5, 
-	      'Jul', 6, 'Aug', 7, 'Sep', 8, 'Oct', 9, 'Nov', 10, 'Dec', 11);
 
 ###### DEFINITION OF AVAILABLE COMMANDS FOR TASKS ######
 
@@ -130,161 +100,6 @@ foreach (keys %asgn_commands) {
     $commands{$_} = $asgn_commands{$_};
 }                                    
 
-#### Spool level subs ####
-##########################
-
-# Initialize Sympaspool global object.
-sub set_spool {
-    $taskspool = new Sympaspool('task');
-}
-
-## Build all Task objects
-sub list_tasks {
-
-    &Log::do_log('debug',"Listing all tasks");
-    my $spool_task = &Conf::get_robot_conf('*','queuetask');
-    ## Reset the list of tasks
-    undef @task_list;
-    undef %task_by_list;
-    undef %task_by_model;
-
-    # fetch all task
-    my $taskspool = new Sympaspool ('task');
-    my @tasks = $taskspool->get_content({'selector'=>{}});
-
-    ## Create Task objects
-    foreach my $t (@tasks) {
-	my $task = new Task ($t);	
-	## Maintain list of tasks
-	push @task_list, $task;
-	
-	my $list_id = $task->{'id'};
-	my $model = $task->{'model'};
-
-	$task_by_model{$model}{$list_id} = $task;
-	$task_by_list{$list_id}{$model} = $task;
-    }    
-    return 1;
-}
-
-## Return a list tasks for the given list
-sub get_tasks_by_list {
-    my $list_id = shift;
-    &Log::do_log('debug',"Getting tasks for list '%s'",$list_id);
-    return () unless (defined $task_by_list{$list_id});
-    return values %{$task_by_list{$list_id}};
-}
-
-## Returns a hash containing the model used. The models returned are all the global models or, if a list name is given as argument, the models used for this list.
-sub get_used_models {
-    ## Optional list parameter
-    my $list_id = shift;
-    &Log::do_log('debug3',"Getting used models for list '%s'",$list_id);
-
-    if (defined $list_id) {
-	if (defined $task_by_list{$list_id}) {
-	    &Log::do_log('debug2',"Found used models for list '%s'",$list_id);
-	    return keys %{$task_by_list{$list_id}}
-	}else {
-	    &Log::do_log('debug2',"Did not find any used models for list '%s'",$list_id);
-	    return ();
-	}
-	
-    }else {
-	return keys %task_by_model;
-    }
-}
-
-## Returns a ref to @task_list, previously defined in the "list_task" sub.
-sub get_task_list {
-    &Log::do_log('debug',"Getting tasks list");
-    return @task_list;
-}
-
-## Checks that all the required tasks at the server level are defined. Create them if needed.
-sub create_required_tasks {
-    my $current_date = shift;
-    &Log::do_log('debug','Creating required tasks from models');
-    my %default_data = ('creation_date' => $current_date, # hash of datas necessary to the creation of tasks
-			'execution_date' => 'execution_date');
-    create_required_global_tasks({'data' => \%default_data,'current_date' => $current_date});
-    create_required_lists_tasks({'data' => \%default_data,'current_date' => $current_date});
-}
-
-## Checks that all the required GLOBAL tasks at the serever level are defined. Create them if needed.
-sub create_required_global_tasks {
-    my $param = shift;
-    my $data = $param->{'data'};
-    &Log::do_log('debug','Creating required tasks from global models');
-
-    my $task;
-    my %used_models; # models for which a task exists
-    foreach my $model (&Task::get_used_models) {
-	$used_models{$model} = 1;
-    }
-    foreach my $key (keys %global_models) {	
-	&Log::do_log('debug2',"global_model : $key");
-	unless ($used_models{$global_models{$key}}) {
-	    if ($Conf::Conf{$key}) {
-		unless($task = create ({'creation_date' => $param->{'current_date'},'model' => $global_models{$key}, 'flavour' => $Conf::Conf{$key}, 'data' =>$data})) {
-		    &Log::do_log('err','Unable to create task with parameters creation_date = "%s", model = "%s", flavour = "%s", data = "%s"',$param->{'current_date'},$global_models{$key},$Conf::Conf{$key},$data);
-		}
-		$used_models{$1} = 1;
-	    }
-	}
-    }
-}
-
-## Checks that all the required LIST tasks are defined. Create them if needed.
-sub create_required_lists_tasks {
-    my $param = shift;
-    &Log::do_log('debug','Creating required tasks from list models');
-
-    my $task;
-    foreach my $robot (keys %{$Conf::Conf{'robots'}}) {
-	&Log::do_log('debug3',"creating list task : current bot  is $robot");
-	my $all_lists = &List::get_lists($robot);
-	foreach my $list ( @$all_lists ) {
-	    &Log::do_log('debug3',"creating list task : current list  is $list->{'name'}");
-	    my %data = %{$param->{'data'}};
-	    $data{'list'} = {'name' => $list->{'name'},
-			     'robot' => $list->{'domain'}};
-	    
-	    my %used_list_models; # stores which models already have a task 
-	    foreach (@list_models) { 
-		$used_list_models{$_} = undef;
-	    }	    
-	    foreach my $model (&Task::get_used_models($list->get_list_id())) {		
-		$used_list_models{$model} = 1; 
-	    }
-	    &Log::do_log('debug3',"creating list task using models");my $tt= 0;
-
-	    foreach my $model (@list_models) {
-		unless ($used_list_models{$model}) {
-		    my $model_task_parameter = "$model".'_task';
-		    
-		    if ( $model eq 'sync_include') {
-			next unless ($list->has_include_data_sources() &&
-				     ($list->{'admin'}{'status'} eq 'open'));
-			unless ($task = create ({'creation_date' => $param->{'current_date'}, 'label' => 'INIT', 'model' => $model, 'flavour' => 'ttl', 'data' => \%data})) {
-			    &Log::do_log('err','Unable to create task with parameters list = "%s", creation_date = "%s", label = "%s", model = "%s", flavour = "%s", data = "%s"',$list->get_list_id,$param->{'current_date'},'INIT',$model,'ttl',\%data);
-			}
-			&Log::do_log('debug3',"sync_include task creation done");$tt++;
-			
-		    }elsif (defined $list->{'admin'}{$model_task_parameter} && 
-			    defined $list->{'admin'}{$model_task_parameter}{'name'} &&
-			    ($list->{'admin'}{'status'} eq 'open')) {
-			unless ($task = create ({'creation_date' => $param->{'current_date'}, 'model' => $model, 'flavour' => $list->{'admin'}{$model_task_parameter}{'name'}, 'data' => \%data})) {
-			    &Log::do_log('err','Unable to create task with parameters list = "%s", creation_date = "%s", model = "%s", flavour = "%s", data = "%s"',$list->get_list_id,$param->{'current_date'},$model,$list->{'admin'}{$model_task_parameter}{'name'},\%data);
-			}
-			$tt++;
-		    }
-		}
-	    }
-	}
-    }
-}
-
 #### Task level subs ####
 ##########################
 
@@ -322,7 +137,6 @@ sub new {
     ## Bless Task object
     bless $task, $pkg;
 
-    return undef unless $task->parse;
     return $task;
 }
 
@@ -348,7 +162,7 @@ sub create {
     }
     my $self = new Task($task_in_spool);
     unless ($self) {
-	&Log::do_log('err','Unable to create list object');
+	&Log::do_log('err','Unable to create task object');
 	return undef;
     }
     $self->{'Rdata'} = $param->{'data'};
@@ -496,6 +310,7 @@ sub store {
 sub remove {
     my $self = shift;
     &Log::do_log('debug',"Removing task '%s'",$self->{'messagekey'});
+    my $taskspool = new Sympaspool('task');
     unless ($taskspool->remove_message({'messagekey'=>$self->{'messagekey'}})){
 	&Log::do_log('err', 'Unable to remove task (messagekey = %s)', $self->{'messagekey'});
 	return undef;
@@ -1016,8 +831,8 @@ sub next_cmd {
 		'execution_date' => 'execution_date');
     if ($listname eq '_global') {
 	$type = '_global';
-	foreach my $key (keys %global_models) {
-	    if ($global_models{$key} eq $model) {
+	foreach my $key (keys %TaskSpool::global_models) {
+	    if ($TaskSpool::global_models{$key} eq $model) {
 		$flavour = $Conf::Conf{$key};
 		last;
 	    }
@@ -1497,7 +1312,7 @@ sub purge_orphan_bounces {
 	 }
 
 	 $date =~ /notAfter=(\w+)\s*(\d+)\s[\d\:]+\s(\d+).+/;
-	 my @date = (0, 0, 0, $2, $months{$1}, $3 - 1900);
+	 my @date = (0, 0, 0, $2, $TaskSpool::months{$1}, $3 - 1900);
 	 $date =~ s/notAfter=//;
 	 my $expiration_date = timegm (@date); # epoch expiration date
 	 my $rep = &tools::adate ($expiration_date);
@@ -1616,7 +1431,7 @@ sub purge_orphan_bounces {
 	 }
 
 	 $date =~ /nextUpdate=(\w+)\s*(\d+)\s(\d\d)\:(\d\d)\:\d\d\s(\d+).+/;
-	 my @date = (0, $4, $3 - 1, $2, $months{$1}, $5 - 1900);
+	 my @date = (0, $4, $3 - 1, $2, $TaskSpool::months{$1}, $5 - 1900);
 	 my $expiration_date = timegm (@date); # epoch expiration date
 	 my $rep = &tools::adate ($expiration_date);
 
