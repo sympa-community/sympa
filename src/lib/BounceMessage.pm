f9e7608cc1c11aabe70813dacada60da77c93a03
david.verdin <david.verdin@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2012-12-13 16:08:54 +0000

    [dev] Refactoring. Handling list definition and redefinition in a BounceMessage instance method. Bounce handling is full of sections where the code tries to guess the list. Embedding list redefinition in a sub prevents multiple List object creation. It also allows to use the instance object $bounce->{'list'} instead of the $list global object. This last replacement is stille to do tomorrow.
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-6.2-branch@8252 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/BounceMessage.pm
+++ src/lib/BounceMessage.pm
@@ -13,7 +13,7 @@ sub new {
     
     my $pkg =shift;
     my $datas = shift;
-	my $self;
+    my $self;
     return undef unless ($self = new Message($datas));
     unless ($self->get_message_as_string) {
 		Log::do_log ('notice',"Ignoring bounce where %s  because it is empty",$self->{'messagekey'});
@@ -22,6 +22,8 @@ sub new {
     bless $self,$pkg;
     $self->{'to'} = $self->get_mime_message->head->get('to', 0);
     chomp $self->{'to'};	
+    $self->{'listname'} = $self->{'list'}{'name'};
+    $self->{'robotname'} = $self->{'robot'}->get_id;
 
     return $self;
 }
@@ -29,6 +31,7 @@ sub new {
 sub analyze_verp_header {
     my $self = shift;
 
+    Log::do_log('trace', "analysing VERP headers for bounce %s",$self->get_msg_id);
     if($self->is_verp_in_use) {
 	if ($self->{'local_part'} =~ /^(.*)(\=\=([wr]))$/) {
 	    $self->{'local_part'} = $1;
@@ -46,9 +49,9 @@ sub analyze_verp_header {
 	$self->{'local_part'} =~ s/\=\=a\=\=/\@/ ;
 	$self->{'local_part'} =~ /^(.*)\=\=(.*)$/ ; 	    
 	$self->{'who'} = $1;
-	$self->{'listname'} = $2;
+	$self->update_list($2,$self->{'robotname'});
 
-	&Log::do_log('notice', 'VERP in use : bounce related to %s for list %s@%s',$self->{'who'},$self->{'listname'},$self->{'domain_part'});
+	&Log::do_log('notice', 'VERP in use : bounce related to %s for list %s@%s',$self->{'who'},$self->{'listname'},$self->{'robotname'});
 	return 1;
     }
     return 0;
@@ -57,10 +60,11 @@ sub analyze_verp_header {
 sub is_verp_in_use {
     my $self = shift;
 
+    Log::do_log('trace', "Checking if VERP is used for bounce %s. to is %s, prefix: %s",$self->get_msg_id,$self->{'to'},Site->bounce_email_prefix);
     my $bounce_email_prefix = Site->bounce_email_prefix;
     if ($self->{'to'} =~ /^$bounce_email_prefix\+(.*)\@(.*)$/) {
 	$self->{'local_part'} = $1;
-	$self->{'domain_part'} = $2;
+	$self->{'robotname'} = $2;
 	return 1;
     }
     return 0;
@@ -69,10 +73,69 @@ sub is_verp_in_use {
 sub failed_on_first_try {
     my $self = shift;
 
+    Log::do_log('trace', "Checking if bounce for message service for bounce %s",$self->get_msg_id);
     if ($self->{'unique'} =~ /[wr]/) {
 	return 1;
     }
     return 0;
+}
+
+sub change_listname {
+    my $self = shift;
+    my $new_listname = shift;
+
+    Log::do_log('trace', "Changing listname from %s to %s for bounce %s",$self->{'listname'},$new_listname,$self->get_msg_id);
+    $self->{'old_listname'} = $self->{'listname'};
+    $self->{'listname'} = $new_listname;
+}
+
+sub change_robotname {
+    my $self = shift;
+    my $new_robotname = shift;
+
+    Log::do_log('trace', "Changing robotname from %s to %s for bounce %s",$self->{'robotname'},$new_robotname,$self->get_msg_id);
+    $self->{'old_robotname'} = $self->{'robotname'};
+    $self->{'robotname'} = $new_robotname;
+}
+
+sub update_list {
+    my $self = shift;
+    my $new_listname = shift;
+    my $new_robotname = shift;
+
+    Log::do_log('trace', "Updating list for bounce %s",$self->get_msg_id);
+    $self->update_robot($new_robotname);
+    $self->change_listname($new_listname);
+
+    if ($self->{'old_listname'} ne $self->{'listname'} || $self->{'old_robotname'} ne $self->{'robotname'}) {
+	my $list = new List ($self->{'listname'}, $self->{'robot'});
+	unless($list) {
+	    Log::do_log('notice','Unable to set list object for unknown list %s@%s (bounce %s)',$self->{'listname'},$self->{'robotname'},$self->{'messagekey'});
+	    return undef;
+	}
+	$self->{'list'} = $list;
+    }
+    
+    return 1;
+}
+
+sub update_robot {
+    my $self = shift;
+    my $new_robotname = shift;
+    
+    Log::do_log('trace', "Updating robot for bounce %s",$self->get_msg_id);
+    $self->change_robotname($new_robotname);
+
+    if ($self->{'old_robotname'} ne $self->{'robotname'}) {
+	my $robot = new Robot($self->{'robot'});
+	unless($robot) {
+	    Log::do_log('notice','Unable to set robot object for unknown robot %s (bounce %s)',$self->{'robotname'},$self->{'messagekey'});
+	    return undef;
+	}
+	$self->{'robot'} = $robot;
+    }
+
+    return 1;
 }
 
 1;
