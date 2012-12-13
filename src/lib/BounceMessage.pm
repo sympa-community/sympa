fa4757332c1055bfea0acad99434b95f3a31d2e6
david.verdin <david.verdin@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2012-12-13 14:05:01 +0000

    [dev] Refactoring. Making analyze_verp_header a BounceMessage instance method. Adding the following instance methods to BounceMessage: - is_verp_in_use: return true if bounce uses VERP and sets the fields necessary to analyse VERP header - failed_on_frist_try: return true if the bounce happenned the first time a message was sent to the receipient.
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-6.2-branch@8245 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/BounceMessage.pm
+++ src/lib/BounceMessage.pm
@@ -20,6 +20,59 @@ sub new {
 		return undef;
     }
     bless $self,$pkg;
+    $self->{'to'} = $self->get_mime_message->head->get('to', 0);
+    chomp $self->{'to'};	
+
     return $self;
 }
 
+sub analyze_verp_header {
+    my $self = shift;
+
+    if($self->is_verp_in_use) {
+	if ($self->{'local_part'} =~ /^(.*)(\=\=([wr]))$/) {
+	    $self->{'local_part'} = $1;
+	    $self->{'unique'} = $2;
+	}elsif ($self->{'local_part'} =~  /^(.*)\=\=a\=\=(.*)\=\=(.*)\=\=(.*)$/ ) {# tracking in use
+	    
+	    $self->{'distribution_id'} = $4;
+	    $self->{'local_part'} =~ /^(.*)\=\=(.*)$/ ;
+	    $self->{'local_part'} = $1;
+	}else{
+	    undef $self->{'distribution_id'} ;
+	    Log::do_log('err', 'NO ID PART in the bounce for :%s', $self->{'to'});
+	}
+
+	$self->{'local_part'} =~ s/\=\=a\=\=/\@/ ;
+	$self->{'local_part'} =~ /^(.*)\=\=(.*)$/ ; 	    
+	$self->{'who'} = $1;
+	$self->{'listname'} = $2;
+
+	&Log::do_log('notice', 'VERP in use : bounce related to %s for list %s@%s',$self->{'who'},$self->{'listname'},$self->{'domain_part'});
+	return 1;
+    }
+    return 0;
+}
+
+sub is_verp_in_use {
+    my $self = shift;
+
+    my $bounce_email_prefix = Site->bounce_email_prefix;
+    if ($self->{'to'} =~ /^$bounce_email_prefix\+(.*)\@(.*)$/) {
+	$self->{'local_part'} = $1;
+	$self->{'domain_part'} = $2;
+	return 1;
+    }
+    return 0;
+}
+
+sub failed_on_first_try {
+    my $self = shift;
+
+    if ($self->{'unique'} =~ /[wr]/) {
+	return 1;
+    }
+    return 0;
+}
+
+1;
