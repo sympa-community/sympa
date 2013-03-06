6c2bf258efbc8b8e7f53c3e12b72c68803d57caa
david.verdin <david.verdin@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2013-03-06 16:31:03 +0000

    [dev] Removing Conf::DAEMON_* methods which were never used. Fixed spooler.pl which would die because it was using the DAEMON_ALL bareword.
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-6.2-branch@8806 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/Conf.pm
+++ src/lib/Conf.pm
@@ -43,7 +43,7 @@ use tools;
 use Lock;
 
 our @ISA = qw(Exporter);
-our @EXPORT = qw(%params %Conf DAEMON_MESSAGE DAEMON_COMMAND DAEMON_CREATION DAEMON_ALL);
+our @EXPORT = qw(%params %Conf);
 
 =head1 NAME
 
@@ -54,11 +54,6 @@ Conf - Sympa configuration
 =head2 CONSTANTS AND EXPORTED VARIABLES
 
 =cut
-
-sub DAEMON_MESSAGE {1};
-sub DAEMON_COMMAND {2};
-sub DAEMON_CREATION {4};
-sub DAEMON_ALL {7};
 
 ## Database and SQL statement handlers
 my $sth;
