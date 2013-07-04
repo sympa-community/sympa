0fe206f0c6ba6a15100a04ad159598181995b951
sikeda <sikeda@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2013-07-04 11:51:42 +0000

    [dev] Changes on List::automatic_tag().  It takes Message object as argument; foldcase & index are used for case-ignore match, instead of regexp.
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-6.2-branch@9454 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/List.pm
+++ src/lib/List.pm
@@ -10001,19 +10001,18 @@ sub is_msg_topic_tagging_required {
 #  Compute the topic(s) of the message and tag it.
 #
 # IN : -$self (+): ref(List)
-#      -$msg (+): ref(MIME::Entity)
-#      -$robot (+): robot
+#      -$message (+): ref(message object)
+#      -$robot (+): *** No longer used
 #
 # OUT : string of tag(s), can be separated by ',', can be empty
 #        | undef
 ####################################################
 sub automatic_tag {
-    my ($self, $msg, $robot) = @_;
-    my $msg_id = $msg->head->get('Message-ID');
-    chomp $msg_id if $msg_id;
-    &Log::do_log('debug3', '(%s, msg_id=%s)', $self, $msg_id);
+    Log::do_log('debug3', '(%s, %s)', @_);
+    my ($self, $message) = @_;
+    my $msg_id = $message->get_msg_id;
 
-    my $topic_list = $self->compute_topic($msg, $robot);
+    my $topic_list = $self->compute_topic($message);
 
     if ($topic_list) {
 	unless ($self->tag_topic($msg_id, $topic_list, 'auto')) {
@@ -10030,42 +10029,39 @@ sub automatic_tag {
 # compute_topic
 ####################################################
 #  Compute the topic of the message. The topic is got
-#  from applying a regexp on the message, regexp
-#  based on keywords defined in list_parameter
-#  msg_topic.keywords. The regexp is applied on the
+#  from keywords defined in list_parameter
+#  msg_topic.keywords. The keyword is applied on the
 #  subject and/or the body of the message according
 #  to list parameter msg_topic_keywords_apply_on
 #
 # IN : -$self (+): ref(List)
-#      -$msg (+): ref(MIME::Entity)
-#      -$robot(+) : robot
+#      -$message (+): ref(message object)
+#      -$robot (+): *** No longer used.
 #
 # OUT : string of tag(s), can be separated by ',', can be empty
 ####################################################
 sub compute_topic {
-    my ($self, $msg, $robot) = @_;
-    my $msg_id = $msg->head->get('Message-ID');
-    chomp $msg_id if $msg_id;
-    Log::do_log('debug3', 'compute_topic(%s, msg_id=%s)', $self, $msg_id);
+    Log::do_log('debug3', '(%s, %s)', @_);
+    my ($self, $message) = @_;
+    my $msg_id = $message->get_msg_id;
+
     my @topic_array;
     my %topic_hash;
     my %keywords;
 
     ## TAGGING INHERITED BY THREAD
-    # getting reply-to
-    my $reply_to = $msg->head->get('In-Reply-To');
-    chomp $reply_to if $reply_to;
+    # getting in-reply-to
+    my $reply_to = $message->get_header('In-Reply-To');
     my $info_msg_reply_to = $self->load_msg_topic($reply_to);
 
     # is msg reply to already tagged?
-    if (ref($info_msg_reply_to) eq "HASH") {
+    if (ref $info_msg_reply_to eq 'HASH') {
 	return $info_msg_reply_to->{'topic'};
     }
 
     ## TAGGING BY KEYWORDS
     # getting keywords
     foreach my $topic (@{$self->msg_topic}) {
-
 	my $list_keyw =
 	    &tools::get_array_from_splitted_string($topic->{'keywords'});
 
@@ -10075,51 +10071,53 @@ sub compute_topic {
     }
 
     # getting string to parse
-    # We convert it to Unicode for case-ignore match with non-ASCII keywords.
+    # We convert it to UTF-8 for case-ignore match with non-ASCII keywords.
     my $mail_string = '';
-    if ($self->msg_topic_keywords_apply_on eq 'subject') {
-	$mail_string =
-	    Encode::decode_utf8(&tools::decode_header($msg, 'Subject')) .
-	    "\n";
+    if (index $self->msg_topic_keywords_apply_on, 'subject') {
+	$mail_string = $message->{'decoded_subject'} . "\n";
     }
     unless ($self->msg_topic_keywords_apply_on eq 'subject') {
 
 	# get bodies of any text/* parts, not digging nested subparts.
 	my @parts;
-	if ($msg->effective_type =~ /^(multipart|message)\//i) {
-	    @parts = $msg->parts();
+	if ($message->as_entity->parts) {
+	    @parts = $message->as_entity->parts;
 	} else {
-	    @parts = ($msg);
+	    @parts = ($message->as_entity);
 	}
 	foreach my $part (@parts) {
 	    next unless $part->effective_type =~ /^text\//i;
 	    my $charset = $part->head->mime_attr("Content-Type.Charset");
 	    $charset = MIME::Charset->new($charset);
+	    $charset->encoder('UTF-8');
+
 	    if (defined $part->bodyhandle) {
 		my $body = $part->bodyhandle->as_string();
 		my $converted;
-		eval { $converted = $charset->decode($body); };
+		eval { $converted = $charset->encode($body); };
 		if ($@) {
-		    $converted = Encode::decode('US-ASCII', $body);
+		    $converted = $body;
+		    $converted =~ s/[^\x01-\x7F]/?/g;
 		}
 		$mail_string .= $converted . "\n";
 	    }
 	}
     }
+    # foldcase string
+    $mail_string = tools::foldcase($mail_string);
 
     # parsing
     foreach my $keyw (keys %keywords) {
 	my $k = $keywords{$keyw};
-	$keyw = Encode::decode_utf8($keyw);
-	$keyw = &tools::escape_regexp($keyw);
-	if ($mail_string =~ /$keyw/i) {
+	$keyw = tools::foldcase($keyw); #foldcase keyword
+	if (index $mail_string, $keyw) {
 	    $topic_hash{$k} = 1;
 	}
     }
 
     # for no double
     foreach my $k (keys %topic_hash) {
-	push @topic_array, $k if ($topic_hash{$k});
+	push @topic_array, $k if $topic_hash{$k};
     }
 
     if ($#topic_array < 0) {
