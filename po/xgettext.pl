317e49c1086be523c34b32248d0368adda635178
sikeda <sikeda@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2014-03-03 06:57:39 +0000

    [dev] Allows keys gettext_id etc. without quotes.
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-6.2-branch@10329 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- po/xgettext.pl
+++ po/xgettext.pl
@@ -297,10 +297,10 @@ foreach my $file (@ordered_files) {
 	&add_expression($expression);
     }	    
 
-	    # Sympa variables (gettext_id and gettext_unit)
+	    # Sympa variables (gettext_comment, gettext_id and gettext_unit)
 	    $line = 1; pos($_) = 0;
-	    while (/\G.*?\'(gettext_comment|gettext_id|gettext_unit)\'\s*=>\s*\"([^\"]+)\"/sg) {
-		my $str = $2;
+	    while (/\G.*?(\'?)(gettext_comment|gettext_id|gettext_unit)\1\s*=>\s*\"([^\"]+)\"/sg) {
+		my $str = $3;
 		$line += ( () = ($& =~ /\n/g) ); # cryptocontext!
 		&add_expression({'expression' => $str,
 				 'filename' => $filename,
@@ -308,8 +308,8 @@ foreach my $file (@ordered_files) {
 	    }
 
 	    $line = 1; pos($_) = 0;
-	    while (/\G.*?\'(gettext_comment|gettext_id|gettext_unit)\'\s*=>\s*\'([^\']+)\'/sg) {
-		my $str = $2;
+	    while (/\G.*?(\'?)(gettext_comment|gettext_id|gettext_unit)\1\s*=>\s*\'([^\']+)\'/sg) {
+		my $str = $3;
 		$line += ( () = ($& =~ /\n/g) ); # cryptocontext!
 		&add_expression({'expression' => $str,
 				 'filename' => $filename,
