4278e1fab7034df0cf741068e0ecd706317c8105
david.verdin <david.verdin@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2015-03-04 09:33:56 +0000

    [-dev] Restoring previous key for stat_counter_table. The composed primary key proposed breaks upgrade for previous install, which don't necessary match the constraints defined by this key.
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-6.2-branch@12015 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/Sympa/DatabaseDescription.pm
+++ src/lib/Sympa/DatabaseDescription.pm
@@ -734,18 +734,17 @@ my %full_db_struct = (
     },
     'stat_counter_table' => {
         'fields' => {
-            #'id_counter' => {
-            #    'struct'   => 'bigint(20)',
-            #    'doc'      => '',
-            #    'order'    => 1,
-            #    'primary'  => 1,
-            #    'not_null' => 1,
-            #},
+            'id_counter' => {
+                'struct'   => 'bigint(20)',
+                'doc'      => '',
+                'order'    => 1,
+                'primary'  => 1,
+                'not_null' => 1,
+            },
             'beginning_date_counter' => {
                 'struct'   => 'int(11)',
                 'doc'      => '',
                 'order'    => 2,
-                'primary'  => 1,
                 'not_null' => 1,
             },
             'end_date_counter' => {
@@ -756,21 +755,18 @@ my %full_db_struct = (
             'data_counter' => {
                 'struct'   => 'varchar(50)',
                 'doc'      => '',
-                'primary'  => 1,
                 'not_null' => 1,
                 'order'    => 3,
             },
             'robot_counter' => {
                 'struct'   => $robot_struct,
                 'doc'      => '',
-                'primary'  => 1,
                 'not_null' => 1,
                 'order'    => 4,
             },
             'list_counter' => {
                 'struct'  => $list_struct,
                 'doc'     => '',
-                'primary' => 1,
                 'order'   => 5,
             },
             #'variation_counter' => {
