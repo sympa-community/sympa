ab060110b166acfb7d9d2a2aad99385532edbf62
sikeda <sikeda@05aa8bb8-cd2b-0410-b1d7-8918dfa770ce>
2015-10-02 06:29:55 +0000

    [-dev] Removing unnecessary upgrading work.
    git-svn-id: https://subversion.renater.fr/sympa/branches/sympa-6.2-branch@12391 05aa8bb8-cd2b-0410-b1d7-8918dfa770ce

--- src/lib/Sympa/Upgrade.pm
+++ src/lib/Sympa/Upgrade.pm
@@ -1712,13 +1712,6 @@ sub upgrade {
             if $status;
     }
 
-    # During 6.2 to 6.2.7, [% static_content_url %] was used but omitted.
-    if (lower_version($previous_version, '6.2.8')
-        and not lower_version($previous_version, '6.2')) {
-        $log->syslog('notice', 'Modifying CSS.');
-        Sympa::Tools::WWW::update_css(force => 1);
-    }
-
     return 1;
 }
 
