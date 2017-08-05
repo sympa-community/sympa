1941489847dc55d15dd5ef8033220892907b187d
IKEDA Soji <ikeda@conversion.co.jp>
2017-08-05 15:19:53 +0900

    Adding new request handler "decl" to decline held requests.

--- src/lib/Sympa/Request/Handler/decl.pm
+++ src/lib/Sympa/Request/Handler/decl.pm
@@ -4,10 +4,6 @@
 
 # Sympa - SYsteme de Multi-Postage Automatique
 #
-# Copyright (c) 1997, 1998, 1999 Institut Pasteur & Christophe Wolfhugel
-# Copyright (c) 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
-# 2006, 2007, 2008, 2009, 2010, 2011 Comite Reseau des Universites
-# Copyright (c) 2011, 2012, 2013, 2014, 2015, 2016, 2017 GIP RENATER
 # Copyright 2017 The Sympa Community. See the AUTHORS.md file at the top-level
 # directory of this distribution and at
 # <https://github.com/sympa-community/sympa.git>.
@@ -25,7 +21,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-package Sympa::Request::Handler::auth;
+package Sympa::Request::Handler::decl;
 
 use strict;
 use warnings;
@@ -54,15 +50,15 @@ sub _twist {
     my $spindle = Sympa::Spindle::ProcessAuth->new(
         (   map { ($req and $req->{$_}) ? ($_ => $req->{$_}) : () }
             qw(context action email)),
-        keyauth      => $key,
-        confirmed_by => $sender,
+        keyauth     => $key,
+        canceled_by => $sender,
 
         scenario_context => $self->{scenario_context},
         stash            => $self->{stash},
     );
 
     unless ($spindle and $spindle->spin) {
-        $log->syslog('info', 'AUTH %s from %s refused, auth failed',
+        $log->syslog('info', 'Declining %s from %s refused, decl failed',
             $key, $sender);
         $self->add_stash($request, 'user', 'wrong_email_confirm',
             {key => $key, command => ($req || {})->{action}});
@@ -81,28 +77,28 @@ __END__
 
 =head1 NAME
 
-Sympa::Request::Handler::auth - auth request handler
+Sympa::Request::Handler::decl - decl request handler
 
 =head1 DESCRIPTION
 
 Fetchs the request matching with {authkey} and optional {request} attributes
 from held request spool,
-and if succeeded, processes it with C<md5> authentication level.
+and if succeeded, remove it from the spool.
 
 =head1 CAVEAT
 
-Auth request handler itself never check privileges:
+Decl request handler itself never check privileges:
 It trust in senders if valid authorization key is specified.
 Access to this handler should be restricted sufficiently by applications.
 
 =head1 SEE ALSO
 
 L<Sympa::Request::Handler>,
-L<Sympa::Request::Handler::decl>,
+L<Sympa::Request::Handler::auth>,
 L<Sympa::Spindle::ProcessAuth>.
 
 =head1 HISTORY
 
-L<Sympa::Request::Handler::auth> appeared on Sympa 6.2.15.
+L<Sympa::Request::Handler::decl> appeared on Sympa 6.2.19b.
 
 =cut
