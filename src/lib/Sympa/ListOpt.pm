2868a5453252061e53cce7f2d64cab8b97747e21
IKEDA Soji <ikeda@conversion.co.jp>
2018-10-12 18:14:15 +0900

    Missed changes.

--- src/lib/Sympa/ListOpt.pm
+++ src/lib/Sympa/ListOpt.pm
@@ -8,8 +8,8 @@
 # Copyright (c) 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005,
 # 2006, 2007, 2008, 2009, 2010, 2011 Comite Reseau des Universites
 # Copyright (c) 2011, 2012, 2013, 2014, 2015, 2016, 2017 GIP RENATER
-# Copyright 2017 The Sympa Community. See the AUTHORS.md file at the top-level
-# directory of this distribution and at
+# Copyright 2017, 2018 The Sympa Community. See the AUTHORS.md file at the
+# top-level directory of this distribution and at
 # <https://github.com/sympa-community/sympa.git>.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -29,8 +29,6 @@ package Sympa::ListOpt;
 
 use strict;
 use warnings;
-
-use Sympa::Robot;
 
 # List parameter values except for parameters below.
 our %list_option = (
@@ -316,7 +314,6 @@ I18n-ed title of option value.
 
 =head1 SEE ALSO
 
-L<Sympa::Language>,
 L<Sympa::ListDef>.
 
 =head1 HISTORY
