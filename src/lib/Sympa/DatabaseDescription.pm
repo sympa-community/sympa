c77b07aa080cac5f3f30ef3fae26c3ae02680904
Olivier Salaün <olivier.salaun@univ-rennes1.fr>
2018-10-31 09:55:36 +0100

    Add index on prev_id_session DB field, see #78

--- src/lib/Sympa/DatabaseDescription.pm
+++ src/lib/Sympa/DatabaseDescription.pm
@@ -1169,7 +1169,8 @@ sub primary {
 our %indexes = (
     'admin_table'      => {'admin_user_index'      => ['user_admin']},
     'subscriber_table' => {'subscriber_user_index' => ['user_subscriber']},
-    'stat_table'       => {'stats_user_index'      => ['email_stat']}
+    'stat_table'       => {'stats_user_index'      => ['email_stat']},
+    'session_table'       => {'session_prev_id_index'      => ['prev_id_session']},
 );
 
 # table indexes that can be removed during upgrade process
