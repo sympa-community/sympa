7d5e17279257099f3c143e77299ca53ef5b760ae
IKEDA Soji <ikeda@conversion.co.jp>
2019-02-20 13:43:14 +0900

    Disable actions related to shared repository if it was disabled.

--- src/lib/Sympa/ConfDef.pm
+++ src/lib/Sympa/ConfDef.pm
@@ -1688,7 +1688,7 @@ our @params = (
             'If set to "on", list owners can open shared repository.',
         'vhost'        => '1',
         'edit'         => '1',
-        'default'      => 'on',
+        'default'      => 'on',    # Enalbed by historical reason.
     },
 
     {   'name'         => 'default_shared_quota',
