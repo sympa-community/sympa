43bfd29458f4022b38e6222ab7674df12406fcff
IKEDA Soji <ikeda@conversion.co.jp>
2020-05-28 16:53:44 +0900

    Refactoring. Sympa::List::on_the_fly_sync_include() was removed. Instead, "include" request handler had "delay" option.

--- src/lib/Sympa/List.pm
+++ src/lib/Sympa/List.pm
@@ -391,21 +391,19 @@ sub new {
     }
 
     ## Config file was loaded or reloaded
-    my $pertinent_ttl = $list->{'admin'}{'distribution_ttl'}
-        || $list->{'admin'}{'ttl'};
     if (    $status
         and grep { $list->{'admin'}{'status'} eq $_ } qw(open pending)
-        and (
-            (   not $options->{'skip_sync_admin'}
-                and $list->_cache_read_expiry('last_sync_admin_user') <
-                time - $pertinent_ttl
-            )
-            or $options->{'force_sync_admin'}
-        )
-    ) {
+        and not $options->{'skip_sync_admin'}) {
         # Update admin_table.
-        $list->sync_include('owner');
-        $list->sync_include('editor');
+        if ($options->{'force_sync_admin'}) {
+            $list->sync_include('owner');
+            $list->sync_include('editor');
+        } else {
+            my $delay = $list->{'admin'}{'distribution_ttl'}
+                // $list->{'admin'}{'ttl'};
+            $list->sync_include('owner',  delay => $delay);
+            $list->sync_include('editor', delay => $delay);
+        }
 
         if (not @{$list->get_admins('owner') || []}
             and $list->{'admin'}{'status'} ne 'error_config') {
@@ -4760,8 +4758,9 @@ sub _load_include_admin_user_file {
 
 sub sync_include {
     $log->syslog('debug2', '(%s, %s)', @_);
-    my $self = shift;
-    my $role = shift;
+    my $self    = shift;
+    my $role    = shift;
+    my %options = @_;
 
     $role ||= 'member';    # Compat.<=6.2.54
 
@@ -4773,6 +4772,7 @@ sub sync_include {
         context          => $self,
         action           => 'include',
         role             => $role,
+        delay            => $options{delay},
         scenario_context => {skip => 1},
     );
     unless ($spindle and $spindle->spin) {
@@ -4806,29 +4806,9 @@ sub sync_include {
 # This one is to be called from anywhere else. This function deletes the
 # scheduled sync_include task. If this deletion happened in sync_include(),
 # it would disturb the normal task_manager.pl functionning.
-#
 # 6.2.4: Returns 0 if synchronization is not needed.
-sub on_the_fly_sync_include {
-    my $self    = shift;
-    my %options = @_;
-
-    my $pertinent_ttl = $self->{'admin'}{'distribution_ttl'}
-        || $self->{'admin'}{'ttl'};
-    $log->syslog('debug2', '(%s)', $pertinent_ttl);
-    if (not $options{'use_ttl'}
-        or $self->_cache_read_expiry('last_sync') < time - $pertinent_ttl) {
-        $log->syslog('notice', "Synchronizing list members...");
-        my $return_value = $self->sync_include('member');
-        $log->syslog('notice', "...done");
-        if ($return_value) {
-            $self->remove_task('sync_include');
-            return 1;
-        } else {
-            return $return_value;
-        }
-    }
-    return 0;
-}
+# No longer used. Use sync_include('member', delay => ...);
+#sub on_the_fly_sync_include;
 
 # DEPRECATED. Use sync_include('owner') & sync_include('editor').
 #sub sync_include_admin;
@@ -6547,35 +6527,8 @@ sub get_next_delivery_date {
 # -> No longer used.
 
 ## Remove a task in the tasks spool
-sub remove_task {
-    my $self = shift;
-    my $task = shift;
-
-    unless (opendir(DIR, $Conf::Conf{'queuetask'})) {
-        $log->syslog(
-            'err',
-            'Can\'t open dir %s: %m',
-            $Conf::Conf{'queuetask'}
-        );
-        return undef;
-    }
-    my @tasks = grep !/^\.\.?$/, readdir DIR;
-    closedir DIR;
-
-    foreach my $task_file (@tasks) {
-        if ($task_file =~
-            /^(\d+)\.\w*\.$task\.$self->{'name'}\@$self->{'domain'}$/) {
-            unless (unlink("$Conf::Conf{'queuetask'}/$task_file")) {
-                $log->syslog('err', 'Unable to remove task file %s: %m',
-                    $task_file);
-                return undef;
-            }
-            $log->syslog('notice', 'Removing task file %s', $task_file);
-        }
-    }
-
-    return 1;
-}
+# No longer used.
+#sub remove_task;
 
 # Deprecated. Use Sympa::Request::Handler::close_list handler.
 #sub close_list;
