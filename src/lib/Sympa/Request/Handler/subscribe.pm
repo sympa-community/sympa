7422e6480048a4fd3294e8583e87d5c6b3007de1
IKEDA Soji <ikeda@conversion.co.jp>
2021-01-23 14:09:52 +0900

    Get rid of meaningless initial password for user account using tmp_passwd()

--- src/lib/Sympa/Request/Handler/subscribe.pm
+++ src/lib/Sympa/Request/Handler/subscribe.pm
@@ -142,8 +142,6 @@ sub _twist {
 
     my $user = Sympa::User->new($email);
     $user->lang($list->{'admin'}{'lang'}) unless $user->lang;
-    $user->password(Sympa::Tools::Password::tmp_passwd($email))
-        unless $user->password;
     $user->save;
 
     ## Now send the welcome file to the user
