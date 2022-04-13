beb427a81ebd38670da25ac17ce48db22bdead87
IKEDA Soji <ikeda@conversion.co.jp>
2022-04-13 14:02:41 +0900

    Small fixes

--- src/lib/Sympa/CLI/rebuildarc.pm
+++ src/lib/Sympa/CLI/rebuildarc.pm
@@ -3,7 +3,7 @@
 
 # Sympa - SYsteme de Multi-Postage Automatique
 #
-# Copyright 2021 The Sympa Community. See the
+# Copyright 2021, 2022 The Sympa Community. See the
 # AUTHORS.md file at the top-level directory of this distribution and at
 # <https://github.com/sympa-community/sympa.git>.
 #
@@ -26,7 +26,6 @@ use strict;
 use warnings;
 
 use Sympa;
-use Sympa::List;
 use Sympa::Message;
 use Sympa::Spool::Archive;
 
