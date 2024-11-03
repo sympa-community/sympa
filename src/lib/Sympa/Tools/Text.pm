d4dd2f7f1ebf1fb3430f0e1e9aba1863ba141add
IKEDA Soji <mail@ikedas.net>
2024-11-03 12:22:09 +0900

    Revert 3a1bbc7 in PR #1885 to postpone making Unicode::UTF8 mandatory.

--- src/lib/Sympa/Tools/Text.pm
+++ src/lib/Sympa/Tools/Text.pm
@@ -39,8 +39,8 @@ use MIME::EncWords;
 use Text::LineFold;
 use Unicode::GCString;
 use URI::Escape qw();
-use Unicode::Normalize qw();
-use Unicode::UTF8;
+BEGIN { eval 'use Unicode::Normalize qw()'; }
+BEGIN { eval 'use Unicode::UTF8 qw()'; }
 
 use Sympa::Language;
 use Sympa::Regexps;
@@ -141,11 +141,15 @@ sub canonic_text {
     my $utext;
     if (Encode::is_utf8($text)) {
         $utext = $text;
-    } else {
+    } elsif ($Unicode::UTF8::VERSION) {
         no warnings 'utf8';
         $utext = Unicode::UTF8::decode_utf8($text);
+    } else {
+        $utext = Encode::decode_utf8($text);
     }
-    $utext = Unicode::Normalize::normalize('NFC', $utext);
+    if ($Unicode::Normalize::VERSION) {
+        $utext = Unicode::Normalize::normalize('NFC', $utext);
+    }
 
     # Remove DOS linefeeds (^M) that cause problems with Outlook 98, AOL,
     # and EIMS:
@@ -309,8 +313,13 @@ sub guessed_to_utf8 {
         and length $text
         and $text =~ /[^\x00-\x7F]/;
 
-    my $utf8 = Unicode::UTF8::decode_utf8($text)
-        if Unicode::UTF8::valid_utf8($text);
+    my $utf8;
+    if ($Unicode::UTF8::VERSION) {
+        $utf8 = Unicode::UTF8::decode_utf8($text)
+            if Unicode::UTF8::valid_utf8($text);
+    } else {
+        $utf8 = eval { Encode::decode_utf8($text, Encode::FB_CROAK()) };
+    }
     unless (defined $utf8) {
         foreach my $charset (map { $_ ? @$_ : () } @legacy_charsets{@langs}) {
             $utf8 =
@@ -323,7 +332,8 @@ sub guessed_to_utf8 {
     }
 
     # Apply NFC: e.g. for modified-NFD by Mac OS X.
-    $utf8 = Unicode::Normalize::normalize('NFC', $utf8);
+    $utf8 = Unicode::Normalize::normalize('NFC', $utf8)
+        if $Unicode::Normalize::VERSION;
 
     return Encode::encode_utf8($utf8);
 }
